d2cda159e1ae3e6204fc5cebb34cfe472f823579
Alexander Neumann <alexander@bumpern.de>
2014-11-30 23:18:58 +0100

    Test incremental backup

--- test/test-backup.sh
+++ test/test-backup.sh
@@ -5,5 +5,11 @@ run khepri init
 run khepri backup "${BASE}/fake-data"
 run khepri restore "$(basename "$KHEPRI_REPOSITORY"/snapshots/*)" "${BASE}/fake-data-restore"
 dirdiff "${BASE}/fake-data" "${BASE}/fake-data-restore/fake-data"
+
+SNAPSHOT=$(run khepri list snapshots)
+run khepri backup "${BASE}/fake-data" $SNAPSHOT
+run khepri restore "$(basename "$KHEPRI_REPOSITORY"/snapshots/*)" "${BASE}/fake-data-restore-incremental"
+dirdiff "${BASE}/fake-data" "${BASE}/fake-data-restore-incremental/fake-data"
+
 run khepri fsck all
 cleanup
