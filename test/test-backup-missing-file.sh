333b514d7b31dd99726f61b39aefece74d4bed5b
Alexander Neumann <alexander@bumpern.de>
2015-01-14 20:20:47 +0100

    Simplify test

--- test/test-backup-missing-file.sh
+++ test/test-backup-missing-file.sh
@@ -13,13 +13,4 @@ rm -f "${BASE}/fake-data/0/0/9/37"
 # resume backup
 fg
 
-# run restic restore "$(basename "$RESTIC_REPOSITORY"/snapshots/*)" "${BASE}/fake-data-restore"
-# dirdiff "${BASE}/fake-data" "${BASE}/fake-data-restore/fake-data"
-
-# SNAPSHOT=$(run restic list snapshots)
-# run restic backup "${BASE}/fake-data" $SNAPSHOT
-# run restic restore "$(basename "$RESTIC_REPOSITORY"/snapshots/*)" "${BASE}/fake-data-restore-incremental"
-# dirdiff "${BASE}/fake-data" "${BASE}/fake-data-restore-incremental/fake-data"
-
-# run restic fsck -o --check-data
 cleanup
