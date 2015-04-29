62a11fa0329b12d7a708a0e88e9b0dc1aa1681f9
Florian Weingarten <flo@hackvalue.de>
2015-04-28 22:09:32 -0400

    merge cache_darwin and cache_linux

--- cache.go
+++ cache.go
@@ -2,6 +2,7 @@ package restic
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -348,4 +349,43 @@ func (c *Cache) LoadMap(s *server.Server, snid backend.ID) (*Map, error) {
 	m := &Map{}
 	err = json.NewDecoder(rd).Decode(m)
 	return m, err
+}
+
+// GetCacheDir returns the cache directory according to XDG basedir spec, see
+// http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+func GetCacheDir() (string, error) {
+	xdgcache := os.Getenv("XDG_CACHE_HOME")
+	home := os.Getenv("HOME")
+
+	if xdgcache == "" && home == "" {
+		return "", errors.New("unable to locate cache directory (XDG_CACHE_HOME and HOME unset)")
+	}
+
+	cachedir := ""
+	if xdgcache != "" {
+		cachedir = filepath.Join(xdgcache, "restic")
+	} else if home != "" {
+		cachedir = filepath.Join(home, ".cache", "restic")
+	}
+
+	fi, err := os.Stat(cachedir)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(cachedir, 0700)
+		if err != nil {
+			return "", err
+		}
+
+		fi, err = os.Stat(cachedir)
+		debug.Log("getCacheDir", "create cache dir %v", cachedir)
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	if !fi.IsDir() {
+		return "", fmt.Errorf("cache dir %v is not a directory", cachedir)
+	}
+
+	return cachedir, nil
 }
