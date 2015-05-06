c86474663c16cd2fd6b2b88db700682c64e89e53
Alexander Neumann <alexander@bumpern.de>
2015-05-06 21:45:23 +0200

    testsuite: Add test for 'ls' command

--- testsuite/test-backup.sh
+++ testsuite/test-backup.sh
@@ -6,10 +6,17 @@ run restic backup "${BASE}/fake-data"
 run restic restore "$(basename "$RESTIC_REPOSITORY"/snapshots/*)" "${BASE}/fake-data-restore"
 dirdiff "${BASE}/fake-data" "${BASE}/fake-data-restore/fake-data"
 
-SNAPSHOT=$(run restic list snapshots)
+SNAPSHOT=$(restic list snapshots)
 run restic backup -p "$SNAPSHOT" "${BASE}/fake-data"
 run restic restore "$(basename "$RESTIC_REPOSITORY"/snapshots/*)" "${BASE}/fake-data-restore-incremental"
 dirdiff "${BASE}/fake-data" "${BASE}/fake-data-restore-incremental/fake-data"
+
+# first line contains snapshot id
+# second line contains timestamp of directory creation
+# so compare from line three on
+echo "snapshot id is $SNAPSHOT"
+restic ls "$SNAPSHOT" | tail -n +3 > "${BASE}/test-ls-output"
+diff -au "${dir}/test-ls-expected" "${BASE}/test-ls-output"
 
 run restic fsck -o --check-data
 cleanup
