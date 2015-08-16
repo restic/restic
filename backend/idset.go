5c46dc41deed4fa79aeab23999b388fddf35f8a0
Alexander Neumann <alexander@bumpern.de>
2015-08-16 16:07:51 +0200

    Add methods to IDSet

--- backend/idset.go
+++ backend/idset.go
@@ -28,3 +28,22 @@ func (s IDSet) Insert(id ID) {
 func (s IDSet) Delete(id ID) {
 	delete(s, id)
 }
+
+// List returns a slice of all IDs in the set.
+func (s IDSet) List() IDs {
+	list := make(IDs, 0, len(s))
+	for id := range s {
+		list = append(list, id)
+	}
+
+	return list
+}
+
+func (s IDSet) String() string {
+	str := s.List().String()
+	if len(str) < 2 {
+		return "{}"
+	}
+
+	return "{" + str[1:len(str)-2] + "}"
+}
