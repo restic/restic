757303245df5792d6d4474e5f45d3725fae14316
Chapuis Bertil <bchapuis@agimem.com>
2015-09-16 16:02:23 +0200

    removed dependency to gorilla mux

--- backend/rest_test.go
+++ backend/rest_test.go
@@ -8,10 +8,10 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
-	"github.com/gorilla/mux"
 	"github.com/restic/restic/backend"
 	"github.com/restic/restic/backend/rest"
 )
@@ -34,111 +34,90 @@ func TestRestBackend(t *testing.T) {
 		os.MkdirAll(filepath.Join(path, d), backend.Modes.Dir)
 	}
 
-	r := mux.NewRouter()
+	r := http.NewServeMux()
 
 	// Check if a configuration exists.
 	r.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
+		method := r.Method
 		file := filepath.Join(path, "config")
-		if _, err := os.Stat(file); err != nil {
-			http.Error(w, "404 repository not found", 404)
+		_, err := os.Stat(file)
+
+		// Check if the config exists
+		if method == "HEAD" && err == nil {
 			return
 		}
-	}).Methods("HEAD")
 
-	// Get the configuration.
-	r.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
-		file := filepath.Join(path, "config")
-		if _, err := os.Stat(file); err != nil {
-			http.Error(w, "404 repository not found", 404)
+		// Get the config
+		if method == "GET" && err == nil {
+			bytes, _ := ioutil.ReadFile(file)
+			w.Write(bytes)
 			return
 		}
-		bytes, _ := ioutil.ReadFile(file)
-		w.Write(bytes)
-	}).Methods("GET")
 
-	// Save the configuration.
-	r.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
-		file := filepath.Join(path, "config")
-		if _, err := os.Stat(file); err == nil {
-			http.Error(w, "409 repository already initialized", 409)
+		// Save the config
+		if method == "POST" && err != nil {
+			bytes, _ := ioutil.ReadAll(r.Body)
+			ioutil.WriteFile(file, bytes, 0600)
 			return
 		}
-		bytes, _ := ioutil.ReadAll(r.Body)
-		ioutil.WriteFile(file, bytes, 0600)
-	}).Methods("POST")
 
-	// List the blobs of a given dir.
-	r.HandleFunc("/{dir}/", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		dir := filepath.Clean(vars["dir"])
-		path := filepath.Join(path, dir)
-		files, _ := ioutil.ReadDir(path)
-		names := make([]string, len(files))
-		for i, f := range files {
-			names[i] = f.Name()
-		}
-		data, _ := json.Marshal(names)
-		w.Write(data)
-	}).Methods("GET")
+		http.Error(w, "404 not found", 404)
+	})
 
-	// Check if a blob of a given dir exists.
-	r.HandleFunc("/{dir}/{name}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		dir := filepath.Clean(vars["dir"])
-		name := filepath.Clean(vars["name"])
-		path := filepath.Join(path, dir, name)
-		if _, err := os.Stat(path); err != nil {
-			http.Error(w, "404 blob not found", 404)
-		}
-	}).Methods("HEAD")
+	for _, dir := range dirs {
 
-	// Get a blob of a given dir.
-	r.HandleFunc("/{dir}/{name}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		dir := filepath.Clean(vars["dir"])
-		name := filepath.Clean(vars["name"])
-		path := filepath.Join(path, dir, name)
-		file, err := os.Open(path)
-		defer file.Close()
-		if err != nil {
-			http.Error(w, "404 blob not found", 404)
-			return
-		}
-		http.ServeContent(w, r, "", time.Unix(0, 0), file)
-	}).Methods("GET")
+		r.HandleFunc("/"+dir+"/", func(w http.ResponseWriter, r *http.Request) {
+			method := r.Method
+			vars := strings.Split(r.RequestURI, "/")
+			name := vars[2]
+			path := filepath.Join(path, dir, name)
+			_, err := os.Stat(path)
 
-	// Save a blob of a given dir.
-	r.HandleFunc("/{dir}/{name}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		dir := filepath.Clean(vars["dir"])
-		name := filepath.Clean(vars["name"])
-		path := filepath.Join(path, dir, name)
-		if _, err := os.Stat(path); err == nil {
-			http.Error(w, "409 blob already uploaded", 409)
-			return
-		}
-		bytes, _ := ioutil.ReadAll(r.Body)
-		ioutil.WriteFile(path, bytes, 0600)
-	}).Methods("POST")
+			// List the blobs of a given dir.
+			if method == "GET" && name == "" && err == nil {
+				files, _ := ioutil.ReadDir(path)
+				names := make([]string, len(files))
+				for i, f := range files {
+					names[i] = f.Name()
+				}
+				data, _ := json.Marshal(names)
+				w.Write(data)
+				return
+			}
 
-	// Delete a blob of a given dir.
-	r.HandleFunc("/{dir}/{name}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		dir := filepath.Clean(vars["dir"])
-		name := filepath.Clean(vars["name"])
-		path := filepath.Join(path, dir, name)
-		if _, err := os.Stat(path); err != nil {
-			http.Error(w, "404 blob not found", 404)
-			return
-		}
-		if err := os.Remove(path); err != nil {
-			http.Error(w, "500 internal server error", 500)
-			return
-		}
-	}).Methods("DELETE")
+			// Check if the blob esists
+			if method == "HEAD" && name != "" && err == nil {
+				return
+			}
+
+			// Get a blob of a given dir.
+			if method == "GET" && name != "" && err == nil {
+				file, _ := os.Open(path)
+				defer file.Close()
+				http.ServeContent(w, r, "", time.Unix(0, 0), file)
+				return
+			}
+
+			// Save a blob
+			if method == "POST" && name != "" && err != nil {
+				bytes, _ := ioutil.ReadAll(r.Body)
+				ioutil.WriteFile(path, bytes, 0600)
+				return
+			}
+
+			// Delete a blob
+			if method == "DELETE" && name != "" && err == nil {
+				os.Remove(path)
+				return
+			}
+
+			http.Error(w, "404 not found", 404)
+		})
+	}
 
 	// Start the server and launch the tests.
 	s := httptest.NewServer(r)
+
 	defer s.Close()
 	u, _ := url.Parse(s.URL)
 	backend, _ := rest.Open(u)
