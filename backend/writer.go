0d5731383f756eb2308cd06435d78009e3f43af9
Alexander Neumann <alexander@bumpern.de>
2015-11-21 17:23:59 +0100

    Remove HashAppendWriter

--- backend/writer.go
+++ backend/writer.go
@@ -1,50 +1,9 @@
 package backend
 
 import (
-	"errors"
 	"hash"
 	"io"
 )
-
-type HashAppendWriter struct {
-	w      io.Writer
-	origWr io.Writer
-	h      hash.Hash
-	sum    []byte
-	closed bool
-}
-
-func NewHashAppendWriter(w io.Writer, h hash.Hash) *HashAppendWriter {
-	return &HashAppendWriter{
-		h:      h,
-		w:      io.MultiWriter(w, h),
-		origWr: w,
-		sum:    make([]byte, 0, h.Size()),
-	}
-}
-
-func (h *HashAppendWriter) Close() error {
-	if h == nil {
-		return nil
-	}
-
-	if !h.closed {
-		h.closed = true
-
-		_, err := h.origWr.Write(h.h.Sum(nil))
-		return err
-	}
-
-	return nil
-}
-
-func (h *HashAppendWriter) Write(p []byte) (n int, err error) {
-	if !h.closed {
-		return h.w.Write(p)
-	}
-
-	return 0, errors.New("Write() called on closed HashAppendWriter")
-}
 
 // HashingWriter wraps an io.Writer to hashes all data that is written to it.
 type HashingWriter struct {
