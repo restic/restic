ffc3503e6f89557d3f3128b872d118301a9f696b
Alexander Neumann <alexander@bumpern.de>
2016-08-01 18:40:08 +0200

    Add first version of FindUsedBlobs

--- src/restic/find.go
+++ src/restic/find.go
@@ -5,8 +5,35 @@ import (
 	"restic/repository"
 )
 
+//  FindUsedBlobs traverse the tree ID and adds all seen blobs to blobs.
+func findUsedBlobs(repo *repository.Repository, treeID backend.ID, blobs backend.IDSet) error {
+	blobs.Insert(treeID)
+
+	tree, err := LoadTree(repo, treeID)
+	if err != nil {
+		return err
+	}
+
+	for _, node := range tree.Nodes {
+		switch node.Type {
+		case "file":
+			for _, blob := range node.Content {
+				blobs.Insert(blob)
+			}
+		case "dir":
+			err := findUsedBlobs(repo, *node.Subtree, blobs)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 // FindUsedBlobs traverses the tree ID and returns a set of all blobs
 // encountered.
-func FindUsedBlobs(repo *repository.Repository, treeID backend.ID) (backend.IDSet, error) {
-	return nil, nil
+func FindUsedBlobs(repo *repository.Repository, treeID backend.ID) (blobs backend.IDSet, err error) {
+	blobs = backend.NewIDSet()
+	return blobs, findUsedBlobs(repo, treeID, blobs)
 }
--- src/restic/find_test.go
+++ src/restic/find_test.go
@@ -1,11 +1,68 @@
 package restic
 
 import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
 	"testing"
 	"time"
 
+	"restic/backend"
 	"restic/repository"
 )
+
+func loadIDSet(t testing.TB, filename string) backend.IDSet {
+	f, err := os.Open(filename)
+	if err != nil {
+		t.Logf("unable to open golden file %v: %v", filename, err)
+		return backend.IDSet{}
+	}
+
+	sc := bufio.NewScanner(f)
+
+	ids := backend.NewIDSet()
+	for sc.Scan() {
+		id, err := backend.ParseID(sc.Text())
+		if err != nil {
+			t.Errorf("file %v contained invalid id: %v", filename, err)
+		}
+
+		ids.Insert(id)
+	}
+
+	if err = f.Close(); err != nil {
+		t.Errorf("closing file %v failed with error %v", filename, err)
+	}
+
+	return ids
+}
+
+func saveIDSet(t testing.TB, filename string, s backend.IDSet) {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatalf("unable to update golden file %v: %v", filename, err)
+		return
+	}
+
+	var ids backend.IDs
+	for id := range s {
+		ids = append(ids, id)
+	}
+
+	sort.Sort(ids)
+	for _, id := range ids {
+		fmt.Fprintf(f, "%s\n", id)
+	}
+
+	if err = f.Close(); err != nil {
+		t.Fatalf("close file %v returned error: %v", filename, err)
+	}
+}
+
+var updateGoldenFiles = flag.Bool("update", false, "update golden files in testdata/")
 
 const (
 	testSnapshots = 3
@@ -25,7 +82,7 @@ func TestFindUsedBlobs(t *testing.T) {
 		snapshots = append(snapshots, sn)
 	}
 
-	for _, sn := range snapshots {
+	for i, sn := range snapshots {
 		usedBlobs, err := FindUsedBlobs(repo, *sn.Tree)
 		if err != nil {
 			t.Errorf("FindUsedBlobs returned error: %v", err)
@@ -37,6 +94,16 @@ func TestFindUsedBlobs(t *testing.T) {
 			continue
 		}
 
-		t.Logf("used blobs from snapshot %v (tree %v): %v", sn.ID().Str(), sn.Tree.Str(), usedBlobs)
+		goldenFilename := filepath.Join("testdata", fmt.Sprintf("used_blobs_snapshot%d", i))
+		want := loadIDSet(t, goldenFilename)
+
+		if !want.Equals(usedBlobs) {
+			t.Errorf("snapshot %d: wrong list of blobs returned:\n  missing blobs: %v\n  extra blobs: %v",
+				i, want.Sub(usedBlobs), usedBlobs.Sub(want))
+		}
+
+		if *updateGoldenFiles {
+			saveIDSet(t, goldenFilename, usedBlobs)
+		}
 	}
 }
