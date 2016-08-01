f1bc181c5b1fa54e22969d3348c9bf6e2fcd23f9
Alexander Neumann <alexander@bumpern.de>
2016-08-01 21:12:23 +0200

    Add more checks for tests

--- src/restic/checker/testing.go
+++ src/restic/checker/testing.go
@@ -20,13 +20,30 @@ func TestCheckRepo(t testing.TB, repo *repository.Repository) {
 
 	done := make(chan struct{})
 	defer close(done)
+
+	// packs
 	errChan := make(chan error)
+	go chkr.Packs(errChan, done)
+
+	for err := range errChan {
+		t.Error(err)
+	}
+
+	// structure
+	errChan = make(chan error)
 	go chkr.Structure(errChan, done)
 
 	for err := range errChan {
 		t.Error(err)
 	}
 
+	// unused blobs
+	blobs := chkr.UnusedBlobs()
+	if len(blobs) > 0 {
+		t.Errorf("unused blobs found: %v", blobs)
+	}
+
+	// read data
 	errChan = make(chan error)
 	go chkr.ReadData(nil, errChan, done)
 
