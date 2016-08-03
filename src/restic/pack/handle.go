1b4b469440bd3f27a5d1931b2a7898afea0c919d
Alexander Neumann <alexander@bumpern.de>
2016-08-03 21:51:20 +0200

    Add pack.Handle and pack.Handles

---
+++ src/restic/pack/handle.go
@@ -0,0 +1,59 @@
+package pack
+
+import (
+	"fmt"
+	"restic/backend"
+)
+
+// Handle identifies a blob of a given type.
+type Handle struct {
+	ID   backend.ID
+	Type BlobType
+}
+
+func (h Handle) String() string {
+	name := h.ID.String()
+	if len(name) > 10 {
+		name = name[:10]
+	}
+	return fmt.Sprintf("<%s/%s>", h.Type, name)
+}
+
+// Handles is an ordered list of Handles that implements sort.Interface.
+type Handles []Handle
+
+func (h Handles) Len() int {
+	return len(h)
+}
+
+func (h Handles) Less(i, j int) bool {
+	if h[i].Type != h[j].Type {
+		return h[i].Type < h[j].Type
+	}
+
+	for k, b := range h[i].ID {
+		if b == h[j].ID[k] {
+			continue
+		}
+
+		if b < h[j].ID[k] {
+			return true
+		}
+
+		return false
+	}
+
+	return false
+}
+
+func (h Handles) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h Handles) String() string {
+	elements := make([]string, 0, len(h))
+	for _, e := range h {
+		elements = append(elements, e.String())
+	}
+	return fmt.Sprintf("%v", elements)
+}
