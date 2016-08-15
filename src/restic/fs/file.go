e5195c73f383140629550bc93a84af6df0a98ab5
Jan Stuertz <jan@bargten.de>
2016-08-15 08:16:11 +0200

    Fix for Linux. Fd() is required on File interface

--- src/restic/fs/file.go
+++ src/restic/fs/file.go
@@ -11,6 +11,7 @@ type File interface {
 	io.Writer
 	io.Closer
 
+        Fd() uintptr
 	Readdirnames(n int) ([]string, error)
 	Readdir(int) ([]os.FileInfo, error)
 	Seek(int64, int) (int64, error)
