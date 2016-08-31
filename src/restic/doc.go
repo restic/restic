e699f6d1bd3973233a99a07e7a3425ee020480b5
Alexander Neumann <alexander@bumpern.de>
2016-08-31 18:27:57 +0200

    Update doc comment

--- src/restic/doc.go
+++ src/restic/doc.go
@@ -1,6 +1,5 @@
 // Package restic is the top level package for the restic backup program,
 // please see https://github.com/restic/restic for more information.
 //
-// This package exposes the main components needed to create and restore a
-// backup as well as handling things like a local cache of objects.
+// This package exposes the main objects that are handled in restic.
 package restic
