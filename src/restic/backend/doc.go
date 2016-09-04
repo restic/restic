ea073f58cf924bcdbfd67039cedd6703491f9c02
Alexander Neumann <alexander@bumpern.de>
2016-09-04 13:08:05 +0200

    Correct comment

--- src/restic/backend/doc.go
+++ src/restic/backend/doc.go
@@ -1,5 +1,4 @@
 // Package backend provides local and remote storage for restic repositories.
-// All backends need to implement the Backend interface. There is a
-// MockBackend, which can be used for mocking in tests, and a MemBackend, which
-// stores all data in a hash internally.
+// All backends need to implement the Backend interface. There is a MemBackend,
+// which stores all data in a map internally and can be used for testing.
 package backend
