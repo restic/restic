032716f1244b6669df57d1d2d1e335f61d52ffc7
jgfrm <jgfrm@gordijn.org>
2017-02-02 16:29:00 +0100

    multi platform issues

--- src/restic/node_windows.go
+++ src/restic/node_windows.go
@@ -24,19 +24,20 @@ func (node Node) restoreSymlinkTimestamps(path string, utimes [2]syscall.Timespe
 
 // Retrieve extended attribute data associated with path.
 func Getxattr(path, name string) ([]byte, error) {
-	return nil
+	return nil, nil
 }
 
 // Retrieves a list of names of extended attributes associated with the
 // given path in the file system.
 func Listxattr(path string) ([]string, error) {
-	return nil
+	return "", nil
 }
 
 // Associates name and data together as an attribute of path.
 func Setxattr(path, name string, data []byte) error {
-	retunr nil
+	return nil, nil
 }
+
 
 type statWin syscall.Win32FileAttributeData
 
