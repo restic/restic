f8dd5d5088a777fb8c66ba88dadd938a76e91c6b
Alexander Neumann <alexander@bumpern.de>
2017-03-09 11:29:24 +0100

    Check version in build.go
    Print a sensible error for Go < 1.7 (the "context" package is missing
    there)

--- build.go
+++ build.go
@@ -291,6 +291,12 @@ func (cs Constants) LDFlags() string {
 }
 
 func main() {
+	ver := runtime.Version()
+	if strings.HasPrefix(ver, "go1") && ver < "go1.7" {
+		fmt.Fprintf(os.Stderr, "Go version %s detected, restic requires at least Go 1.7\n", ver)
+		os.Exit(1)
+	}
+
 	buildTags := []string{}
 
 	skipNext := false
