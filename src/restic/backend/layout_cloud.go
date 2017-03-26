3e81dcdfc27060d756fb5c3e5f613aa262e321e5
Alexander Neumann <alexander@bumpern.de>
2017-03-26 22:20:10 +0200

    Add cloud and s3 layout

---
+++ src/restic/backend/layout_cloud.go
@@ -0,0 +1,36 @@
+package backend
+
+import "restic"
+
+// CloudLayout implements the default layout for cloud storage backends, as
+// described in the Design document.
+type CloudLayout struct {
+	Path string
+	Join func(...string) string
+}
+
+var cloudLayoutPaths = defaultLayoutPaths
+
+// Dirname returns the directory path for a given file type and name.
+func (l *CloudLayout) Dirname(h restic.Handle) string {
+	return l.Join(l.Path, cloudLayoutPaths[h.Type])
+}
+
+// Filename returns a path to a file, including its name.
+func (l *CloudLayout) Filename(h restic.Handle) string {
+	name := h.Name
+
+	if h.Type == restic.ConfigFile {
+		name = "config"
+	}
+
+	return l.Join(l.Dirname(h), name)
+}
+
+// Paths returns all directory names
+func (l *CloudLayout) Paths() (dirs []string) {
+	for _, p := range cloudLayoutPaths {
+		dirs = append(dirs, l.Join(l.Path, p))
+	}
+	return dirs
+}
--- src/restic/backend/layout_test.go
+++ src/restic/backend/layout_test.go
@@ -64,7 +64,143 @@ func TestDefaultLayout(t *testing.T) {
 		sort.Sort(sort.StringSlice(dirs))
 
 		if !reflect.DeepEqual(dirs, want) {
-			t.Fatalf("wrong paths returned, want:\v  %v\ngot:\n  %v", want, dirs)
+			t.Fatalf("wrong paths returned, want:\n  %v\ngot:\n  %v", want, dirs)
+		}
+	})
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v/%v", test.Type, test.Handle.Name), func(t *testing.T) {
+			filename := l.Filename(test.Handle)
+			if filename != test.filename {
+				t.Fatalf("wrong filename, want %v, got %v", test.filename, filename)
+			}
+		})
+	}
+}
+
+func TestCloudLayout(t *testing.T) {
+	path, cleanup := test.TempDir(t)
+	defer cleanup()
+
+	var tests = []struct {
+		restic.Handle
+		filename string
+	}{
+		{
+			restic.Handle{Type: restic.DataFile, Name: "0123456"},
+			filepath.Join(path, "data", "0123456"),
+		},
+		{
+			restic.Handle{Type: restic.ConfigFile, Name: "CFG"},
+			filepath.Join(path, "config"),
+		},
+		{
+			restic.Handle{Type: restic.SnapshotFile, Name: "123456"},
+			filepath.Join(path, "snapshots", "123456"),
+		},
+		{
+			restic.Handle{Type: restic.IndexFile, Name: "123456"},
+			filepath.Join(path, "index", "123456"),
+		},
+		{
+			restic.Handle{Type: restic.LockFile, Name: "123456"},
+			filepath.Join(path, "locks", "123456"),
+		},
+		{
+			restic.Handle{Type: restic.KeyFile, Name: "123456"},
+			filepath.Join(path, "keys", "123456"),
+		},
+	}
+
+	l := &CloudLayout{
+		Path: path,
+		Join: filepath.Join,
+	}
+
+	t.Run("Paths", func(t *testing.T) {
+		dirs := l.Paths()
+
+		want := []string{
+			filepath.Join(path, "data"),
+			filepath.Join(path, "snapshots"),
+			filepath.Join(path, "index"),
+			filepath.Join(path, "locks"),
+			filepath.Join(path, "keys"),
+		}
+
+		sort.Sort(sort.StringSlice(want))
+		sort.Sort(sort.StringSlice(dirs))
+
+		if !reflect.DeepEqual(dirs, want) {
+			t.Fatalf("wrong paths returned, want:\n  %v\ngot:\n  %v", want, dirs)
+		}
+	})
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v/%v", test.Type, test.Handle.Name), func(t *testing.T) {
+			filename := l.Filename(test.Handle)
+			if filename != test.filename {
+				t.Fatalf("wrong filename, want %v, got %v", test.filename, filename)
+			}
+		})
+	}
+}
+
+func TestS3Layout(t *testing.T) {
+	path, cleanup := test.TempDir(t)
+	defer cleanup()
+
+	var tests = []struct {
+		restic.Handle
+		filename string
+	}{
+		{
+			restic.Handle{Type: restic.DataFile, Name: "0123456"},
+			filepath.Join(path, "data", "0123456"),
+		},
+		{
+			restic.Handle{Type: restic.ConfigFile, Name: "CFG"},
+			filepath.Join(path, "config"),
+		},
+		{
+			restic.Handle{Type: restic.SnapshotFile, Name: "123456"},
+			filepath.Join(path, "snapshot", "123456"),
+		},
+		{
+			restic.Handle{Type: restic.IndexFile, Name: "123456"},
+			filepath.Join(path, "index", "123456"),
+		},
+		{
+			restic.Handle{Type: restic.LockFile, Name: "123456"},
+			filepath.Join(path, "lock", "123456"),
+		},
+		{
+			restic.Handle{Type: restic.KeyFile, Name: "123456"},
+			filepath.Join(path, "key", "123456"),
+		},
+	}
+
+	l := &S3Layout{
+		Path: path,
+		Join: filepath.Join,
+	}
+
+	t.Run("Paths", func(t *testing.T) {
+		dirs := l.Paths()
+
+		want := []string{
+			filepath.Join(path, "data"),
+			filepath.Join(path, "snapshot"),
+			filepath.Join(path, "index"),
+			filepath.Join(path, "lock"),
+			filepath.Join(path, "key"),
+		}
+
+		sort.Sort(sort.StringSlice(want))
+		sort.Sort(sort.StringSlice(dirs))
+
+		if !reflect.DeepEqual(dirs, want) {
+			t.Fatalf("wrong paths returned, want:\n  %v\ngot:\n  %v", want, dirs)
 		}
 	})
 
