e2af5890f39e427bb51e92e699f87d5632c42577
Alexander Neumann <alexander@bumpern.de>
2017-04-10 23:21:04 +0200

    backend: Add test for listing files with layouts

--- src/restic/backend/local/layout_test.go
+++ src/restic/backend/local/layout_test.go
@@ -2,6 +2,7 @@ package local
 
 import (
 	"path/filepath"
+	"restic"
 	. "restic/test"
 	"testing"
 )
@@ -14,10 +15,23 @@ func TestLayout(t *testing.T) {
 		filename        string
 		layout          string
 		failureExpected bool
+		datafiles       map[string]bool
 	}{
-		{"repo-layout-local.tar.gz", "", false},
-		{"repo-layout-cloud.tar.gz", "", false},
-		{"repo-layout-s3-old.tar.gz", "", false},
+		{"repo-layout-local.tar.gz", "", false, map[string]bool{
+			"aa464e9fd598fe4202492ee317ffa728e82fa83a1de1a61996e5bd2d6651646c": false,
+			"fc919a3b421850f6fa66ad22ebcf91e433e79ffef25becf8aef7c7b1eca91683": false,
+			"c089d62788da14f8b7cbf77188305c0874906f0b73d3fce5a8869050e8d0c0e1": false,
+		}},
+		{"repo-layout-cloud.tar.gz", "", false, map[string]bool{
+			"fc919a3b421850f6fa66ad22ebcf91e433e79ffef25becf8aef7c7b1eca91683": false,
+			"c089d62788da14f8b7cbf77188305c0874906f0b73d3fce5a8869050e8d0c0e1": false,
+			"aa464e9fd598fe4202492ee317ffa728e82fa83a1de1a61996e5bd2d6651646c": false,
+		}},
+		{"repo-layout-s3-old.tar.gz", "", false, map[string]bool{
+			"fc919a3b421850f6fa66ad22ebcf91e433e79ffef25becf8aef7c7b1eca91683": false,
+			"c089d62788da14f8b7cbf77188305c0874906f0b73d3fce5a8869050e8d0c0e1": false,
+			"aa464e9fd598fe4202492ee317ffa728e82fa83a1de1a61996e5bd2d6651646c": false,
+		}},
 	}
 
 	for _, test := range tests {
@@ -35,6 +49,33 @@ func TestLayout(t *testing.T) {
 
 			if be == nil {
 				t.Fatalf("Open() returned nil but no error")
+			}
+
+			datafiles := make(map[string]bool)
+			for id := range be.List(restic.DataFile, nil) {
+				datafiles[id] = false
+			}
+
+			if len(datafiles) == 0 {
+				t.Errorf("List() returned zero data files")
+			}
+
+			for id := range test.datafiles {
+				if _, ok := datafiles[id]; !ok {
+					t.Errorf("datafile with id %v not found", id)
+				}
+
+				datafiles[id] = true
+			}
+
+			for id, v := range datafiles {
+				if !v {
+					t.Errorf("unexpected id %v found", id)
+				}
+			}
+
+			if err = be.Close(); err != nil {
+				t.Errorf("Close() returned error %v", err)
 			}
 
 			RemoveAll(t, filepath.Join(path, "repo"))
