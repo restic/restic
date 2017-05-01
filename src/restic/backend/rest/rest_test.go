554013ca9fd28c252042dedb128b3d4d4c9d8aa4
Alexander Neumann <alexander@bumpern.de>
2017-05-01 23:08:16 +0200

    rest: Convert to test suite

--- src/restic/backend/rest/rest_test.go
+++ src/restic/backend/rest/rest_test.go
@@ -1,39 +1,99 @@
 package rest_test
 
 import (
-	"fmt"
+	"bufio"
+	"io/ioutil"
 	"net/url"
 	"os"
+	"os/exec"
 	"restic"
+	"strings"
+	"testing"
 
 	"restic/backend/rest"
 	"restic/backend/test"
 	. "restic/test"
 )
 
-//go:generate go run ../test/generate_backend_tests.go
-
-func init() {
-	if TestRESTServer == "" {
-		SkipMessage = "REST test server not available"
-		return
-	}
-
-	url, err := url.Parse(TestRESTServer)
+func runRESTServer(t testing.TB, dir string) func() {
+	srv, err := exec.LookPath("rest-server")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid url: %v\n", err)
-		return
+		t.Skip(err)
 	}
 
-	cfg := rest.Config{
-		URL: url,
+	cmd := exec.Command(srv, "--path", dir)
+	cmd.Stdout = os.Stdout
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	test.CreateFn = func() (restic.Backend, error) {
-		return rest.Create(cfg)
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
 	}
 
-	test.OpenFn = func() (restic.Backend, error) {
-		return rest.Open(cfg)
+	sc := bufio.NewScanner(stderr)
+	for sc.Scan() {
+		if strings.HasPrefix(sc.Text(), "Starting server") {
+			break
+		}
 	}
+
+	return func() {
+		if err := cmd.Process.Kill(); err != nil {
+			t.Fatal(err)
+		}
+
+		// ignore errors, we've killed the process
+		_ = cmd.Wait()
+	}
+}
+
+func TestBackend(t *testing.T) {
+	dir, cleanup := TempDir(t)
+	defer cleanup()
+
+	cleanup = runRESTServer(t, dir)
+	defer cleanup()
+
+	suite := test.Suite{
+		// NewConfig returns a config for a new temporary backend that will be used in tests.
+		NewConfig: func() (interface{}, error) {
+			dir, err := ioutil.TempDir(TestTempDir, "restic-test-rest-")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			t.Logf("create new backend at %v", dir)
+
+			url, err := url.Parse("http://localhost:8000/restic-test")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			cfg := rest.Config{
+				URL: url,
+			}
+			return cfg, nil
+		},
+
+		// CreateFn is a function that creates a temporary repository for the tests.
+		Create: func(config interface{}) (restic.Backend, error) {
+			cfg := config.(rest.Config)
+			return rest.Create(cfg)
+		},
+
+		// OpenFn is a function that opens a previously created temporary repository.
+		Open: func(config interface{}) (restic.Backend, error) {
+			cfg := config.(rest.Config)
+			return rest.Open(cfg)
+		},
+
+		// CleanupFn removes data created during the tests.
+		Cleanup: func(config interface{}) error {
+			return nil
+		},
+	}
+
+	suite.RunTests(t)
 }
