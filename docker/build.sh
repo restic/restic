7cad84a90f92bd51d03aa16b25c2d8bc1826cc94
Thomas LEVEIL <thomasleveil@gmail.com>
2017-11-06 23:09:20 +0100

    [docker] allow to run the docker build script from anywhere

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# figure out the directory this script sits in
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
 
 echo "Build docker image restic/restic:latest"
-docker build --rm -t restic/restic:latest -f docker/Dockerfile .
+docker build --rm -t restic/restic:latest -f "$dir/Dockerfile" "$dir/.."
