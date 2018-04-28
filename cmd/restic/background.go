1449d7dc29eae36954c1190f57004a1f2b35e09f
Alexander Neumann <alexander@bumpern.de>
2018-04-28 14:36:24 +0200

    Remove background checking code

--- cmd/restic/background.go
+++
@@ -1,9 +0,0 @@
-// +build !linux
-
-package main
-
-// IsProcessBackground should return true if it is running in the background or false if not
-func IsProcessBackground() bool {
-	//TODO: Check if the process are running in the background in other OS than linux
-	return false
-}
