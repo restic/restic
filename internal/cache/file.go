5dd0df0162b2dca58f4b3c650bbb46ae06a3adf8
Alexander Neumann <alexander@bumpern.de>
2018-08-11 22:47:01 +0200

    cache: Remove files from cache which are too small

--- internal/cache/file.go
+++ internal/cache/file.go
@@ -63,6 +63,12 @@ func (c *Cache) Load(h restic.Handle, length int, offset int64) (io.ReadCloser, 
 		return nil, errors.Errorf("cached file %v is truncated, removing", h)
 	}
 
+	if fi.Size() < offset+int64(length) {
+		_ = f.Close()
+		_ = c.Remove(h)
+		return nil, errors.Errorf("cached file %v is too small, removing", h)
+	}
+
 	if offset > 0 {
 		if _, err = f.Seek(offset, io.SeekStart); err != nil {
 			_ = f.Close()
