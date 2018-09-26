7e39c355e8c16316ca9c80741bf756f40a9be72c
Alexander Neumann <alexander@bumpern.de>
2018-09-26 14:45:09 +0200

    Add testing scaffolding and a few test cases

--- internal/restic/snapshot_test.go
+++ internal/restic/snapshot_test.go
@@ -1,16 +1,142 @@
-package restic_test
+package restic
 
 import (
 	"testing"
 	"time"
 
-	"github.com/restic/restic/internal/restic"
+	"github.com/google/go-cmp/cmp"
 	rtest "github.com/restic/restic/internal/test"
 )
 
 func TestNewSnapshot(t *testing.T) {
 	paths := []string{"/home/foobar"}
 
-	_, err := restic.NewSnapshot(paths, nil, "foo", time.Now(), "", 0)
+	_, err := NewSnapshot(paths, nil, "foo", time.Now(), "", 0)
 	rtest.OK(t, err)
+}
+
+func TestPathSplit(t *testing.T) {
+	var tests = []struct {
+		Input     string
+		Root      string
+		Remainder []string
+	}{
+		{
+			"foo",
+			"foo",
+			[]string{},
+		},
+		{
+			"foo/bar",
+			"foo",
+			[]string{"bar"},
+		},
+		{
+			"/home/user/work",
+			"/home",
+			[]string{"user", "work"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			root, rest := pathSplit(test.Input)
+			if root != test.Root {
+				t.Errorf("wrong root path returned, want %q, got %q", test.Root, root)
+			}
+
+			if !cmp.Equal(test.Remainder, rest) {
+				t.Fatal(cmp.Diff(test.Remainder, rest))
+			}
+		})
+	}
+}
+
+func TestPathMangle(t *testing.T) {
+	var tests = []struct {
+		Path   string
+		Prefix string
+		Strip  int
+		Result string
+	}{
+		{
+			Path:   "foo",
+			Result: "foo",
+		},
+		{
+			Path:   "/home/user",
+			Result: "/home/user",
+		},
+		{
+			Path:   "../home/user",
+			Result: "../home/user",
+		},
+		{
+			Path:   "foo/bar",
+			Strip:  1,
+			Result: "bar",
+		},
+		{
+			Path:   "foo/bar",
+			Strip:  2,
+			Result: "",
+		},
+		{
+			Path:   "foo/bar",
+			Strip:  3,
+			Result: "",
+		},
+		{
+			Path:   "/home/user/foo/bar",
+			Strip:  1,
+			Result: "user/foo/bar",
+		},
+		{
+			Path:   "/home/user/foo/bar",
+			Strip:  2,
+			Result: "foo/bar",
+		},
+		{
+			Path:   "/home/user/foo/bar",
+			Strip:  3,
+			Result: "bar",
+		},
+		{
+			Path:   "/home/user/foo/bar",
+			Prefix: "/srv/backup",
+			Result: "/srv/backup/home/user/foo/bar",
+		},
+		{
+			Path:   "../user/foo/bar",
+			Prefix: "/srv/backup",
+			Result: "/srv/user/foo/bar",
+		},
+		{
+			Path:   "/home/user/foo/bar",
+			Strip:  1,
+			Prefix: "/srv/backup",
+			Result: "/srv/backup/user/foo/bar",
+		},
+		{
+			Path:   "/home/user/foo/bar",
+			Strip:  2,
+			Prefix: "/srv/backup",
+			Result: "/srv/backup/foo/bar",
+		},
+		{
+			Path:   "../user/foo/bar",
+			Strip:  1,
+			Prefix: "/srv/backup",
+			Result: "/srv/user/user/foo/bar",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			res := pathMangle(test.Path, test.Prefix, test.Strip)
+			if res != test.Result {
+				t.Fatalf("wrong result, want %q, got %q", res, test.Result)
+			}
+		})
+	}
 }
