63647e93e4cce342294133d4d6d96ca6525db051
Johannes Hertenstein <johannes@nimius.net>
2018-10-05 22:13:49 +0200

    Escape subcommand in docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Build binary using golang docker image"
 docker run --rm -ti \
-    -v `pwd`:/go/src/github.com/restic/restic \
+    -v "`pwd`":/go/src/github.com/restic/restic \
     -w /go/src/github.com/restic/restic golang:1.8.3-alpine go run build.go
 
 echo "Build docker image restic/restic:latest"
