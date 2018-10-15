f8286263727b588999a19d7786a8a4e44d8b208d
Igor Fedorenko <igor@ifedorenko.com>
2018-10-15 10:34:24 -0400

    WIP template-based progress ui api
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- internal/restorer/progress.go
+++ internal/restorer/progress.go
@@ -1,9 +1,6 @@
 package restorer
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/restic/restic/internal/ui"
 )
 
@@ -17,16 +14,18 @@ type progressUI struct {
 	ui ui.ProgressUI
 
 	// directories are eagerly created
-	totalDirs uint
+	totalDirs ui.Counter
 
 	// files are selected first, then restored, then optionally verified
-	totalFiles                   uint
-	totalBytes                   uint64
+	totalFiles                   ui.Counter
+	totalBytes                   ui.Counter
 	restoredFiles, verifiedFiles ui.CounterTo
 	restoredBytes, verifiedBytes ui.CounterTo
 
+	// TODO track downloaded bytes
+
 	// only count hardlinks and special files
-	totalHardlinks, totalSpecialFiles uint
+	totalHardlinks, totalSpecialFiles ui.Counter
 
 	restoredMetadata ui.CounterTo
 }
@@ -37,35 +36,35 @@ func newProgressUI(ui ui.ProgressUI) *progressUI {
 
 // call when a directory is created
 func (p *progressUI) addDir() {
-	p.ui.Update(func() { p.totalDirs++ })
+	p.ui.Update(func() { (&p.totalDirs).Add(1) })
 }
 
 // call when a file is selected
 func (p *progressUI) addFile(size uint64) {
 	p.ui.Update(func() {
-		p.totalFiles++
-		p.totalBytes += size
+		(&p.totalFiles).Add(1)
+		(&p.totalBytes).Add(int64(size))
 	})
 }
 
 // call when a hardlink is selected
 func (p *progressUI) addHardlink() {
-	p.ui.Update(func() { p.totalHardlinks++ })
+	p.ui.Update(func() { p.totalHardlinks.Add(1) })
 }
 
 // call when a special file is selected
 func (p *progressUI) addSpecialFile() {
-	p.ui.Update(func() { p.totalSpecialFiles++ })
+	p.ui.Update(func() { (&p.totalSpecialFiles).Add(1) })
 }
 
 // call when a file blob is written to a file
 func (p *progressUI) completeBlob(size uint) {
-	p.ui.Update(func() { p.restoredBytes.Add(size) })
+	p.ui.Update(func() { (&p.restoredBytes).Add(int64(size)) })
 }
 
 // call when a file blob is verified
 func (p *progressUI) completeVerifyBlob(size uint) {
-	p.ui.Update(func() { p.verifiedBytes.Add(size) })
+	p.ui.Update(func() { p.verifiedBytes.Add(int64(size)) })
 }
 
 // call when a file is verified
@@ -95,108 +94,62 @@ func (p *progressUI) completeMetadata() {
 
 // announce start of file listing phase
 func (p *progressUI) startFileListing() {
-	start := time.Now()
 	setup := func() {}
-	progress := func() string {
-		progress := fmt.Sprintf("%d directories, %s %d files",
-			p.totalDirs,
-			ui.FormatBytes(p.totalBytes),
-			p.totalFiles)
-		return progress
+	metrics := map[string]interface{}{
+		"dirs":  &p.totalDirs,
+		"files": &p.totalFiles,
+		"bytes": &p.totalBytes,
 	}
-	subtotal := func() string {
-		summary := fmt.Sprintf("Created %d directories, listed %s in %d files in %s",
-			p.totalDirs,
-			ui.FormatBytes(p.totalBytes),
-			p.totalFiles,
-			ui.FormatDurationSince(start))
-		return summary
-	}
+	progress := "{{.dirs.Value}} directories, {{.bytes.FormatBytes}} in {{.files.Value}} files"
+	subtotal := "Created {{.dirs.Value}} directories, listed {{.bytes.FormatBytes}} in {{.files.Value}} files"
 
-	p.ui.Set("Creating directories and listing files...", setup, nil, progress, subtotal)
+	p.ui.Set("Creating directories and listing files...", setup, metrics, progress, subtotal)
 }
 
 // announce start of file content download phase
 func (p *progressUI) startFileContent() {
-	start := time.Now()
 	setup := func() {
-		p.restoredFiles = ui.StartCountTo(start, uint64(p.totalFiles))
-		p.restoredBytes = ui.StartCountTo(start, p.totalBytes)
+		p.restoredFiles = ui.StartCountTo(p.totalFiles.Value())
+		p.restoredBytes = ui.StartCountTo(p.totalBytes.Value())
 	}
-	eta := func() time.Duration {
-		return p.restoredBytes.ETA(time.Now())
+	metrics := map[string]interface{}{
+		"files":   &p.restoredFiles,
+		"bytes":   &p.restoredBytes,
+		"percent": &p.restoredBytes,
 	}
-	progress := func() string {
-		progress := fmt.Sprintf("%s %s / %s %d / %d files",
-			p.restoredBytes.FormatPercent(),
-			ui.FormatBytes(uint64(p.restoredBytes.Value())),
-			ui.FormatBytes(p.totalBytes),
-			p.restoredFiles.Value(),
-			p.totalFiles)
-		return progress
-	}
-	subtotal := func() string {
-		summary := fmt.Sprintf("Restored %s in %d files in %s",
-			ui.FormatBytes(uint64(p.restoredBytes.Value())),
-			p.restoredFiles.Value(),
-			ui.FormatDurationSince(start))
-		return summary
-	}
-	p.ui.Set("Restoring files content...", setup, eta, progress, subtotal)
+	progress := "{{.bytes.FormatBytes}} / {{.bytes.Target.FormatBytes}} {{.files.Value}} / {{.files.Target.Value}} files"
+	subtotal := "Restored {{.files.FormatBytes}} in {{.files.Value}} files"
+	p.ui.Set("Restoring files content...", setup, metrics, progress, subtotal)
 }
 
 // announce filesystem metadata restoration phase
 func (p *progressUI) startMetadata() {
-	start := time.Now()
 	setup := func() {
-		p.restoredMetadata = ui.StartCountTo(start, uint64(p.totalDirs+p.totalFiles+p.totalHardlinks+p.totalSpecialFiles))
+		p.restoredMetadata = ui.StartCountTo(p.totalDirs.Value() + p.totalFiles.Value() + p.totalHardlinks.Value() + p.totalSpecialFiles.Value())
 	}
-	eta := func() time.Duration {
-		return p.restoredMetadata.ETA(time.Now())
+	metrics := map[string]interface{}{
+		"metadata": &p.restoredMetadata,
+		"percent":  &p.restoredMetadata,
 	}
-	progress := func() string {
-		progress := fmt.Sprintf("%s %d / %d timestamps",
-			p.restoredMetadata.FormatPercent(),
-			p.restoredMetadata.Value(),
-			p.restoredMetadata.Target())
-		return progress
-	}
-	subtotal := func() string {
-		summary := fmt.Sprintf("Restored %d filesystem timestamps and other metadata in %s",
-			p.restoredMetadata.Value(),
-			ui.FormatDurationSince(start))
-		return summary
-	}
-	p.ui.Set("Restoring filesystem timestamps and other metadata...", setup, eta, progress, subtotal)
+	progress := "{{.metadata.Value}} / {{.metadata.Target.Value}}"
+	subtotal := "Restored {{.metadata.Value}} filesystem timestamps and other metadata"
+	p.ui.Set("Restoring filesystem timestamps and other metadata...", setup, metrics, progress, subtotal)
 }
 
 // announce start of file content verification phase
 func (p *progressUI) startVerify() {
-	start := time.Now()
 	setup := func() {
-		p.verifiedFiles = ui.StartCountTo(start, uint64(p.totalFiles))
-		p.verifiedBytes = ui.StartCountTo(start, p.totalBytes)
+		p.verifiedFiles = ui.StartCountTo(p.totalFiles.Value())
+		p.verifiedBytes = ui.StartCountTo(p.totalBytes.Value())
 	}
-	eta := func() time.Duration {
-		return p.verifiedBytes.ETA(time.Now())
+	metrics := map[string]interface{}{
+		"files":   &p.verifiedFiles,
+		"bytes":   &p.verifiedBytes,
+		"percent": &p.verifiedBytes,
 	}
-	progress := func() string {
-		progress := fmt.Sprintf("%s %s / %s %d / %d files",
-			p.verifiedBytes.FormatPercent(),
-			ui.FormatBytes(p.verifiedBytes.Value()),
-			ui.FormatBytes(p.totalBytes),
-			p.verifiedFiles.Value(),
-			p.totalFiles)
-		return progress
-	}
-	subtotal := func() string {
-		summary := fmt.Sprintf("Verified %s in %d files in %s",
-			ui.FormatBytes(p.verifiedBytes.Value()),
-			p.verifiedFiles.Value(),
-			ui.FormatDurationSince(start))
-		return summary
-	}
-	p.ui.Set("Verifying files content...", setup, eta, progress, subtotal)
+	progress := ""
+	subtotal := "Verified {{.bytes.FormatBytes}} in {{.files.Value}} files"
+	p.ui.Set("Verifying files content...", setup, metrics, progress, subtotal)
 }
 
 // show restore summary and statistics
