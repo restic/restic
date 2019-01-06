a439cdeb05b6b73f35a55fd802d833388d84d95c
Alexander Neumann <alexander@bumpern.de>
2019-01-06 21:40:32 +0100

    Update manpages and auto-completion

--- doc/zsh-completion.zsh
+++ doc/zsh-completion.zsh
@@ -7,7 +7,7 @@ case $state in
   level1)
     case $words[1] in
       restic)
-        _arguments '1: :(backup cache cat check diff dump find forget generate help init key list ls migrate mount options prune rebuild-index restore self-update snapshots stats tag unlock version)'
+        _arguments '1: :(backup cache cat check diff dump find forget generate help init key list ls migrate mount options prune rebuild-index recover restore self-update snapshots stats tag unlock version)'
       ;;
       *)
         _arguments '*: :_files'
