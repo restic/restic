13c40d4199e852deb6b9a9445344c6731bfbfc95
Vincent Bernat <vincent@bernat.ch>
2019-07-02 21:11:38 +0200

    filter: additional tests for filter.List()

--- internal/filter/filter_test.go
+++ internal/filter/filter_test.go
@@ -248,6 +248,7 @@ var filterListTests = []struct {
 }{
 	{[]string{}, "/foo/bar/test.go", false, false},
 	{[]string{"*.go"}, "/foo/bar/test.go", true, true},
+	{[]string{"*.go"}, "/foo/bar", false, true},
 	{[]string{"*.c"}, "/foo/bar/test.go", false, true},
 	{[]string{"*.go", "*.c"}, "/foo/bar/test.go", true, true},
 	{[]string{"*"}, "/foo/bar/test.go", true, true},
@@ -255,8 +256,12 @@ var filterListTests = []struct {
 	{[]string{"?"}, "/foo/bar/test.go", false, true},
 	{[]string{"?", "x"}, "/foo/bar/x", true, true},
 	{[]string{"/*/*/bar/test.*"}, "/foo/bar/test.go", false, false},
+	{[]string{"/*/*/bar/test.*"}, "/foo/bar/bar", false, true},
 	{[]string{"/*/*/bar/test.*", "*.go"}, "/foo/bar/test.go", true, true},
 	{[]string{"", "*.c"}, "/foo/bar/test.go", false, true},
+	{[]string{"/foo/bar/*"}, "/foo", false, true},
+	{[]string{"/foo/**/test.c"}, "/foo/bar/foo/bar/test.c", true, true},
+	{[]string{"/foo/*/test.c"}, "/foo/bar/foo/bar/test.c", false, false},
 }
 
 func TestList(t *testing.T) {
