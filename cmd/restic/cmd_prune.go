fab4110186b24fd9d832994336eca6c62b5ce95f
Michael Eischer <michael.eischer@fau.de>
2020-03-26 22:30:21 +0100

    prune: Point bug report link to the bug issue template
    The current link only showed a blank issue.

--- cmd/restic/cmd_prune.go
+++ cmd/restic/cmd_prune.go
@@ -210,7 +210,7 @@ func pruneRepository(gopts GlobalOptions, repo restic.Repository) error {
 	if len(usedBlobs) > stats.blobs {
 		return errors.Fatalf("number of used blobs is larger than number of available blobs!\n" +
 			"Please report this error (along with the output of the 'prune' run) at\n" +
-			"https://github.com/restic/restic/issues/new")
+			"https://github.com/restic/restic/issues/new?template=Bug.md")
 	}
 
 	Verbosef("found %d of %d data blobs still in use, removing %d blobs\n",
