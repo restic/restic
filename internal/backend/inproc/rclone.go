3f9b003404c3c8a0848ae81892c8bcc73f0ad82f
Jack <jackdeng@gmail.com>
2020-07-25 08:51:18 -0700

    move roundtripper out of rclone

--- internal/backend/inproc/rclone.go
+++ internal/backend/inproc/rclone.go
@@ -4,17 +4,31 @@ package inproc
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"strings"
 
 	_ "github.com/rclone/rclone/backend/all"
+	"github.com/rclone/rclone/cmd"
+	"github.com/rclone/rclone/cmd/serve/httplib/httpflags"
 	"github.com/rclone/rclone/cmd/serve/restic"
 )
 
 type rcloneProvider struct {
 }
 
+type server struct {
+	*restic.Server
+}
+
 func (rp *rcloneProvider) NewService(args string) (http.RoundTripper, error) {
-	return restic.NewServer(strings.Fields(args))
+	f := cmd.NewFsSrc(strings.Fields(args))
+	return &server{restic.NewServer(f, &httpflags.Opt)}, nil
+}
+
+func (s *server) RoundTrip(r *http.Request) (*http.Response, error) {
+	w := httptest.NewRecorder()
+	s.ServeHTTP(w, r)
+	return w.Result(), nil
 }
 
 func init() {
