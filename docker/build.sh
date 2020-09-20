090a73f7c5de21d743b3dc8a04ee000e48884886
J0WI <J0WI@users.noreply.github.com>
2020-09-20 15:42:12 +0000

    Update Go version to 1.15 in Docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ set -e
 echo "Build binary using golang docker image"
 docker run --rm -ti \
     -v "`pwd`":/go/src/github.com/restic/restic \
-    -w /go/src/github.com/restic/restic golang:1.14.6-alpine go run build.go
+    -w /go/src/github.com/restic/restic golang:1.15-alpine go run build.go
 
 echo "Build docker image restic/restic:latest"
 docker build --rm -t restic/restic:latest -f docker/Dockerfile .
