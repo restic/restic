feedf0ebce832626e5179088a7a9729c6b4ab8e9
greatroar <@>
2020-10-05 16:03:51 +0200

    Don't read password for generate, help or self-update
    Fixes #2951.

--- cmd/restic/main.go
+++ cmd/restic/main.go
@@ -51,7 +51,7 @@ directories in an encrypted repository stored on different backends.
 			return err
 		}
 		globalOptions.extended = opts
-		if c.Name() == "version" {
+		if !needsPassword(c.Name()) {
 			return nil
 		}
 		pwd, err := resolvePassword(globalOptions, "RESTIC_PASSWORD")
@@ -69,6 +69,18 @@ directories in an encrypted repository stored on different backends.
 
 		return nil
 	},
+}
+
+// Distinguish commands that need the password from those that work without,
+// so we don't run $RESTIC_PASSWORD_COMMAND for no reason (it might prompt the
+// user for authentication).
+func needsPassword(cmd string) bool {
+	switch cmd {
+	case "cache", "generate", "help", "options", "self-update", "version":
+		return false
+	default:
+		return true
+	}
 }
 
 var logBuffer = bytes.NewBuffer(nil)
