c193cea1194592bc7dfaa19d2bbe5a680b2b70d7
Alexander Neumann <alexander@bumpern.de>
2020-10-10 15:24:26 +0200

    Pass Context to NewRestorer()

--- internal/restorer/restorer_test.go
+++ internal/restorer/restorer_test.go
@@ -758,7 +758,7 @@ func TestRestorerConsistentTimestampsAndPermissions(t *testing.T) {
 		},
 	})
 
-	res, err := NewRestorer(repo, id)
+	res, err := NewRestorer(context.TODO(), repo, id)
 	rtest.OK(t, err)
 
 	res.SelectFilter = func(item string, dstpath string, node *restic.Node) (selectedForRestore bool, childMayBeSelected bool) {
