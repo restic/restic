85fe5feadbf7b4135bf5880af8e4148c932eb66d
Michael Eischer <michael.eischer@fau.de>
2020-12-29 14:56:14 +0100

    Unify progress report frequency calculation

--- cmd/restic/progress.go
+++ cmd/restic/progress.go
@@ -9,12 +9,10 @@ import (
 	"github.com/restic/restic/internal/ui/progress"
 )
 
-// newProgressMax returns a progress.Counter that prints to stdout.
-func newProgressMax(show bool, max uint64, description string) *progress.Counter {
-	if !show {
-		return nil
-	}
-
+// calculateProgressInterval returns the interval configured via RESTIC_PROGRESS_FPS
+// or if unset returns an interval for 60fps on interactive terminals and 0 (=disabled)
+// for non-interactive terminals
+func calculateProgressInterval() time.Duration {
 	interval := time.Second / 60
 	fps, err := strconv.ParseInt(os.Getenv("RESTIC_PROGRESS_FPS"), 10, 64)
 	if err == nil && fps >= 1 {
@@ -25,6 +23,15 @@ func newProgressMax(show bool, max uint64, description string) *progress.Counter
 	} else if !stdoutIsTerminal() {
 		interval = 0
 	}
+	return interval
+}
+
+// newProgressMax returns a progress.Counter that prints to stdout.
+func newProgressMax(show bool, max uint64, description string) *progress.Counter {
+	if !show {
+		return nil
+	}
+	interval := calculateProgressInterval()
 
 	return progress.New(interval, func(v uint64, d time.Duration, final bool) {
 		status := fmt.Sprintf("[%s] %s  %d / %d %s",
