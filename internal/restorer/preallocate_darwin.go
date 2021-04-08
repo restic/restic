bb7e35671f8a3872a04a6460876a134bd3c25dde
greatroar <@>
2021-04-08 20:42:06 +0200

    Fix runtime.KeepAlive usage in preallocateFile
    When fst is a struct, what runtime.KeepAlive receives is a copy pointed
    to by an interface{}. If I've got the unsafe.Pointer rules right, the
    compiler is allowed to decide that neither FcntlInt call can see the
    struct because all it they get is an int. It's then allowed to reorder
    the code to first copy the struct, then drop the memory for the
    original, then call FcntlInt.
    
    In any case, this matches the example in the runtime package more
    closely.

--- internal/restorer/preallocate_darwin.go
+++ internal/restorer/preallocate_darwin.go
@@ -10,13 +10,13 @@ import (
 
 func preallocateFile(wr *os.File, size int64) error {
 	// try contiguous first
-	fst := unix.Fstore_t{
+	fst := &unix.Fstore_t{
 		Flags:   unix.F_ALLOCATECONTIG | unix.F_ALLOCATEALL,
 		Posmode: unix.F_PEOFPOSMODE,
 		Offset:  0,
 		Length:  size,
 	}
-	_, err := unix.FcntlInt(wr.Fd(), unix.F_PREALLOCATE, int(uintptr(unsafe.Pointer(&fst))))
+	_, err := unix.FcntlInt(wr.Fd(), unix.F_PREALLOCATE, int(uintptr(unsafe.Pointer(fst))))
 
 	if err == nil {
 		return nil
@@ -24,7 +24,7 @@ func preallocateFile(wr *os.File, size int64) error {
 
 	// just take preallocation in any form, but still ask for everything
 	fst.Flags = unix.F_ALLOCATEALL
-	_, err = unix.FcntlInt(wr.Fd(), unix.F_PREALLOCATE, int(uintptr(unsafe.Pointer(&fst))))
+	_, err = unix.FcntlInt(wr.Fd(), unix.F_PREALLOCATE, int(uintptr(unsafe.Pointer(fst))))
 
 	// Keep struct alive until fcntl has returned
 	runtime.KeepAlive(fst)
