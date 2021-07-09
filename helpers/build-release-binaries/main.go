495831d53cf4191ce24c9d4bf9fd29473cd18655
Michael Eischer <michael.eischer@fau.de>
2021-07-10 00:10:24 +0200

    add release binaries for linux/s390x

--- helpers/build-release-binaries/main.go
+++ helpers/build-release-binaries/main.go
@@ -226,7 +226,7 @@ var defaultBuildTargets = map[string][]string{
 	"aix":     {"ppc64"},
 	"darwin":  {"amd64", "arm64"},
 	"freebsd": {"386", "amd64", "arm"},
-	"linux":   {"386", "amd64", "arm", "arm64", "ppc64le", "mips", "mipsle", "mips64", "mips64le"},
+	"linux":   {"386", "amd64", "arm", "arm64", "ppc64le", "mips", "mipsle", "mips64", "mips64le", "s390x"},
 	"netbsd":  {"386", "amd64"},
 	"openbsd": {"386", "amd64"},
 	"windows": {"386", "amd64"},
