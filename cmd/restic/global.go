594f155eb6faf57dd02508283f8d84dfa4c125a7
Alexander Neumann <alexander@bumpern.de>
2022-04-10 10:58:55 +0200

    Add version for 0.13.1

--- cmd/restic/global.go
+++ cmd/restic/global.go
@@ -40,7 +40,7 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-var version = "0.13.0"
+var version = "0.13.1"
 
 // TimeFormat is the format used for all timestamps printed by restic.
 const TimeFormat = "2006-01-02 15:04:05"
