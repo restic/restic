dcb00fd2d16084e51566beb69ab76df8a12bde92
Michael Eischer <michael.eischer@fau.de>
2022-05-22 17:31:37 +0200

    archiver: cleanup Saver interface

--- internal/archiver/blob_saver.go
+++ internal/archiver/blob_saver.go
@@ -11,7 +11,6 @@ import (
 // Saver allows saving a blob.
 type Saver interface {
 	SaveBlob(ctx context.Context, t restic.BlobType, data []byte, id restic.ID, storeDuplicate bool) (restic.ID, bool, int, error)
-	Index() restic.MasterIndex
 }
 
 // BlobSaver concurrently saves incoming blobs to the repo.
