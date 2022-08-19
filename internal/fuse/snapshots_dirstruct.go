af50fe9ac0c3f0222ac4dc9169a72f3a215ab6c6
Michael Eischer <michael.eischer@fau.de>
2022-08-19 18:17:57 +0200

    mount: Map slashes in tags to underscores
    Suggested-by: greatroar <>

--- internal/fuse/snapshots_dirstruct.go
+++ internal/fuse/snapshots_dirstruct.go
@@ -97,6 +97,7 @@ func pathsFromSn(pathTemplate string, timeTemplate string, sn *restic.Snapshot) 
 				// needs special treatment: Rebuild the string builders
 				newout := make([]strings.Builder, len(out)*len(sn.Tags))
 				for i, tag := range sn.Tags {
+					tag = filenameFromTag(tag)
 					for j := range out {
 						newout[i*len(out)+j].WriteString(out[j].String() + tag)
 					}
@@ -137,6 +138,24 @@ func pathsFromSn(pathTemplate string, timeTemplate string, sn *restic.Snapshot) 
 	}
 
 	return paths, timeSuffix
+}
+
+// Some tags are problematic when used as filenames:
+//
+//	""
+//	".", ".."
+//	anything containing '/'
+//
+// Replace all special character by underscores "_", an empty tag is also represented as a underscore.
+func filenameFromTag(tag string) string {
+	switch tag {
+	case "", ".":
+		return "_"
+	case "..":
+		return "__"
+	}
+
+	return strings.ReplaceAll(tag, "/", "_")
 }
 
 // determine static path prefix
