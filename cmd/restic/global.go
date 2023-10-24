27ec320eae3c2a2f106b6581e0a0a121a2f77d2d
Alexander Neumann <alexander@bumpern.de>
2023-10-24 20:02:58 +0200

    Set development version for 0.16.1

--- cmd/restic/global.go
+++ cmd/restic/global.go
@@ -43,7 +43,7 @@ import (
 	"golang.org/x/term"
 )
 
-var version = "0.16.1"
+var version = "0.16.1-dev (compiled manually)"
 
 // TimeFormat is the format used for all timestamps printed by restic.
 const TimeFormat = "2006-01-02 15:04:05"
