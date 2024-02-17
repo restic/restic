fe68d2cafb1098b7bee16983be57492ee91388cc
Michael Eischer <michael.eischer@fau.de>
2024-02-17 21:41:07 +0100

    add feature flag documentation

--- cmd/restic/cmd_features.go
+++ cmd/restic/cmd_features.go
@@ -10,13 +10,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// FIXME explain semantics
-
 var featuresCmd = &cobra.Command{
 	Use:   "features",
 	Short: "Print list of feature flags",
 	Long: `
 The "features" command prints a list of supported feature flags.
+
+To pass feature flags to restic, set the RESTIC_FEATURES environment variable
+to "featureA=true,featureB=false". Specifying an unknown feature flag is an error.
+
+A feature can either be in alpha, beta, stable or deprecated state.
+An _alpha_ feature is disabled by default and may change in arbitrary ways between restic versions or be removed.
+A _beta_ feature is enabled by default, but still can change in minor ways or be removed.
+A _stable_ feature is always enabled and cannot be disabled. The flag will be removed in a future restic version.
+A _deprecated_ feature is always disabled and cannot be enabled. The flag will be removed in a future restic version.
 
 EXIT STATUS
 ===========
