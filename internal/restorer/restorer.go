d4be734c73565f822a4965c835bf302330631464
Aneesh Nireshwalia <99904+aneesh-n@users.noreply.github.com>
2024-02-22 17:54:43 -0700

    Handle readonly empty files in windows

--- internal/restorer/restorer.go
+++ internal/restorer/restorer.go
@@ -205,11 +205,19 @@ func (res *Restorer) restoreHardlinkAt(node *restic.Node, target, path, location
 
 func (res *Restorer) restoreEmptyFileAt(node *restic.Node, target, location string) error {
 	wr, err := os.OpenFile(target, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
-	if err != nil {
-		return err
+	if fs.IsAccessDenied(err) {
+		// If file is readonly, clear the readonly flag by resetting the
+		// permissions of the file and try again
+		// as the metadata will be set again in the second pass and the
+		// readonly flag will be applied again if needed.
+		if err = fs.ResetPermissions(target); err != nil {
+			return err
+		}
+		if wr, err = os.OpenFile(target, os.O_TRUNC|os.O_WRONLY, 0600); err != nil {
+			return err
+		}
 	}
-	err = wr.Close()
-	if err != nil {
+	if err = wr.Close(); err != nil {
 		return err
 	}
 
