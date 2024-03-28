a9b3d86c4f4d90a7b48ee02da6bf328b95ef4332
Michael Eischer <michael.eischer@fau.de>
2024-03-28 18:44:45 +0100

    features: remove example feature

--- internal/feature/registry.go
+++ internal/feature/registry.go
@@ -5,14 +5,12 @@ var Flag = New()
 
 // flag names are written in kebab-case
 const (
-	ExampleFeature       FlagName = "example-feature"
 	DeprecateLegacyIndex FlagName = "deprecate-legacy-index"
 	DeviceIDForHardlinks FlagName = "device-id-for-hardlinks"
 )
 
 func init() {
 	Flag.SetFlags(map[FlagName]FlagDesc{
-		ExampleFeature:       {Type: Alpha, Description: "just for testing"},
 		DeprecateLegacyIndex: {Type: Beta, Description: "disable support for index format used by restic 0.1.0. Use `restic repair index` to update the index if necessary."},
 		DeviceIDForHardlinks: {Type: Alpha, Description: "store deviceID only for hardlinks to reduce metadata changes for example when using btrfs subvolumes. Will be removed in a future restic version after repository format 3 is available"},
 	})
