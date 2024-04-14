eda9f7beb45931b68f1844cdf2516ba4f5c6fe4b
Michael Eischer <michael.eischer@fau.de>
2024-04-14 11:44:13 +0200

    ui/progress: add helper to print messages during tests

--- internal/ui/progress/printer.go
+++ internal/ui/progress/printer.go
@@ -1,5 +1,7 @@
 package progress
 
+import "testing"
+
 // A Printer can can return a new counter or print messages
 // at different log levels.
 // It must be safe to call its methods from concurrent goroutines.
@@ -28,3 +30,36 @@ func (*NoopPrinter) P(_ string, _ ...interface{}) {}
 func (*NoopPrinter) V(_ string, _ ...interface{}) {}
 
 func (*NoopPrinter) VV(_ string, _ ...interface{}) {}
+
+// TestPrinter prints messages during testing
+type TestPrinter struct {
+	t testing.TB
+}
+
+func NewTestPrinter(t testing.TB) *TestPrinter {
+	return &TestPrinter{
+		t: t,
+	}
+}
+
+var _ Printer = (*TestPrinter)(nil)
+
+func (p *TestPrinter) NewCounter(_ string) *Counter {
+	return nil
+}
+
+func (p *TestPrinter) E(msg string, args ...interface{}) {
+	p.t.Logf("error: "+msg, args...)
+}
+
+func (p *TestPrinter) P(msg string, args ...interface{}) {
+	p.t.Logf("print: "+msg, args...)
+}
+
+func (p *TestPrinter) V(msg string, args ...interface{}) {
+	p.t.Logf("verbose: "+msg, args...)
+}
+
+func (p *TestPrinter) VV(msg string, args ...interface{}) {
+	p.t.Logf("verbose2: "+msg, args...)
+}
