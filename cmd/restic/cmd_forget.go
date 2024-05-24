3cc6827f09f75f94904cbfeb3f1497af6510a56a
Michael Eischer <michael.eischer@fau.de>
2024-05-24 20:41:19 +0200

    forget: add feature flag for safe --keep-tags behavior

--- cmd/restic/cmd_forget.go
+++ cmd/restic/cmd_forget.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/restic/restic/internal/errors"
+	"github.com/restic/restic/internal/feature"
 	"github.com/restic/restic/internal/restic"
 	"github.com/restic/restic/internal/ui/termstatus"
 	"github.com/spf13/cobra"
@@ -258,7 +259,7 @@ func runForget(ctx context.Context, opts ForgetOptions, pruneOptions PruneOption
 
 			keep, remove, reasons := restic.ApplyPolicy(snapshotGroup, policy)
 
-			if !policy.Empty() && len(keep) == 0 {
+			if feature.Flag.Enabled(feature.SafeForgetKeepTags) && !policy.Empty() && len(keep) == 0 {
 				return fmt.Errorf("refusing to delete last snapshot of snapshot group \"%v\"", key.String())
 			}
 			if len(keep) != 0 && !gopts.Quiet && !gopts.JSON {
