03be64a094c88284ee3bdf36cc2cd382e820517f
Michael Eischer <michael.eischer@fau.de>
2024-08-03 22:17:07 +0200

    mount: enable debug log for TestMountSameTimestamps
    The test has started to become flaky.

--- cmd/restic/cmd_mount_integration_test.go
+++ cmd/restic/cmd_mount_integration_test.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	systemFuse "github.com/anacrolix/fuse"
+	"github.com/restic/restic/internal/debug"
 	"github.com/restic/restic/internal/restic"
 	rtest "github.com/restic/restic/internal/test"
 )
@@ -203,6 +204,11 @@ func TestMount(t *testing.T) {
 func TestMountSameTimestamps(t *testing.T) {
 	if !rtest.RunFuseTest {
 		t.Skip("Skipping fuse tests")
+	}
+
+	debugEnabled := debug.TestLogToStderr(t)
+	if debugEnabled {
+		defer debug.TestDisableLog(t)
 	}
 
 	env, cleanup := withTestEnvironment(t)
