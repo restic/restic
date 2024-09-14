15fa98abb5c3058faa7f66a224ab4a0145f50d1d
Michael Eischer <michael.eischer@fau.de>
2024-09-14 19:09:58 +0200

    repair snapshots: remove irregular files

--- cmd/restic/cmd_repair_snapshots.go
+++ cmd/restic/cmd_repair_snapshots.go
@@ -92,6 +92,10 @@ func runRepairSnapshots(ctx context.Context, gopts GlobalOptions, opts RepairOpt
 	// - files whose contents are not fully available  (-> file will be modified)
 	rewriter := walker.NewTreeRewriter(walker.RewriteOpts{
 		RewriteNode: func(node *restic.Node, path string) *restic.Node {
+			if node.Type == restic.NodeTypeIrregular || node.Type == restic.NodeTypeInvalid {
+				Verbosef("  file %q: removed node with invalid type %q\n", path, node.Type)
+				return nil
+			}
 			if node.Type != restic.NodeTypeFile {
 				return node
 			}
