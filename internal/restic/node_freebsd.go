3c82fe6ef54909fd5fb8773e0e557e23ba1e6189
greatroar <61184462+greatroar@users.noreply.github.com>
2024-10-03 21:17:22 +0200

    fs: Include filename in mknod errors

--- internal/restic/node_freebsd.go
+++ internal/restic/node_freebsd.go
@@ -3,14 +3,21 @@
 
 package restic
 
-import "syscall"
+import (
+	"os"
+	"syscall"
+)
 
 func (node Node) restoreSymlinkTimestamps(path string, utimes [2]syscall.Timespec) error {
 	return nil
 }
 
-func mknod(path string, mode uint32, dev uint64) (err error) {
-	return syscall.Mknod(path, mode, dev)
+func mknod(path string, mode uint32, dev uint64) error {
+	err := syscall.Mknod(path, mode, dev)
+	if err != nil {
+		err = &os.PathError{Op: "mknod", Path: path, Err: err}
+	}
+	return err
 }
 
 func (s statT) atim() syscall.Timespec { return s.Atimespec }
