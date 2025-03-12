e745e3c3b5eba5231e0fc6664ff00cc8fc527b77
Winfried Plappert <18740761+wplapper@users.noreply.github.com>
2025-03-12 17:38:26 +0000

    restic list blobs - integration test:
    correct code in parseIDsFromReader which always assumed there would only be one item per line, a restic.ID.
    This is incorrect in the case of 'list blobs'.
    
    Updated faulty code.
    Added a test which compares the output from 'repo.ListBlobs', collecting IDs only with
    the output from 'restic list blobs'.

--- cmd/restic/cmd_list_integration_test.go
+++ cmd/restic/cmd_list_integration_test.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"context"
 	"io"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/restic/restic/internal/restic"
@@ -23,11 +25,22 @@ func parseIDsFromReader(t testing.TB, rd io.Reader) restic.IDs {
 	IDs := restic.IDs{}
 	sc := bufio.NewScanner(rd)
 
+	var id restic.ID
+	var err error
 	for sc.Scan() {
-		id, err := restic.ParseID(sc.Text())
-		if err != nil {
-			t.Logf("parse id %v: %v", sc.Text(), err)
-			continue
+		if len(sc.Text()) == 64 {
+			id, err = restic.ParseID(sc.Text())
+			if err != nil {
+				t.Logf("parse id %v: %v", sc.Text(), err)
+				continue
+			}
+		} else {
+			parts := strings.Split(sc.Text(), " ")
+			id, err = restic.ParseID(parts[1])
+			if err != nil {
+				t.Logf("parse id %v: %v", sc.Text(), err)
+				continue
+			}
 		}
 
 		IDs = append(IDs, id)
@@ -41,4 +54,36 @@ func testListSnapshots(t testing.TB, opts GlobalOptions, expected int) restic.ID
 	snapshotIDs := testRunList(t, "snapshots", opts)
 	rtest.Assert(t, len(snapshotIDs) == expected, "expected %v snapshot, got %v", expected, snapshotIDs)
 	return snapshotIDs
+}
+
+func TestListBlobs(t *testing.T) {
+
+	env, cleanup := withTestEnvironment(t)
+	defer cleanup()
+
+	datafile := testSetupBackupData(t, env)
+	_ = datafile
+	opts := BackupOptions{}
+
+	// first backup
+	testRunBackup(t, "", []string{filepath.Join(env.testdata, "0", "0", "7")}, opts, env.gopts)
+	testListSnapshots(t, env.gopts, 1)
+
+	resticIDs := testRunList(t, "blobs", env.gopts)
+	testIDSet := restic.NewIDSet(resticIDs...)
+
+	// get repo
+	_, repo, unlock, err := openWithReadLock(context.TODO(), env.gopts, false)
+	rtest.OK(t, err)
+	defer unlock()
+
+	setFromIndex := restic.IDSet{}
+	// make sure the index is loaded
+	rtest.OK(t, repo.LoadIndex(context.TODO(), nil))
+	// get first tree blob
+	rtest.OK(t, repo.ListBlobs(context.TODO(), func(blob restic.PackedBlob) {
+		setFromIndex.Insert(blob.ID)
+	}))
+
+	rtest.Assert(t, setFromIndex.Equals(testIDSet), "the set of restic.ID s should be equal")
 }
