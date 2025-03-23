cfa3c5884dd1e42bd01a442919756d82cb76c1b6
Martin Smith <Martin2112@users.github.com>
2025-03-23 10:16:20 +0000

    Fix lock pass by value and handle error from Release().

--- internal/restic/lock_windows.go
+++ internal/restic/lock_windows.go
@@ -14,12 +14,15 @@ func uidGidInt(_ *user.User) (uid, gid uint32, err error) {
 
 // checkProcess will check if the process retaining the lock exists.
 // Returns true if the process exists.
-func (l Lock) processExists() bool {
+func (l *Lock) processExists() bool {
 	proc, err := os.FindProcess(l.PID)
 	if err != nil {
 		debug.Log("error searching for process %d: %v\n", l.PID, err)
 		return false
 	}
-	proc.Release()
+	err = proc.Release()
+	if err != nil {
+		debug.Log("error releasing process %d: %v\n", l.PID, err)
+	}
 	return true
 }
