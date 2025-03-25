1637d96d620eb02db36367c2fae3d2de2dad44b7
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2025-03-25 15:03:13 -0400

    backend/sftp: Add sftp.server-alive-interval and sftp.server-alive-count-max options
    Restic previously did not set the `ServerAliveInterval` and `ServerAliveCountMax`
    SSH options when connecting to an SFTP server, resulting in backups being
    interrupted on some connections. It now sets these options to the values
    specified by the `sftp.server-alive-interval` and `sftp.server-alive-count-max`
    configuration options, to avoid connections being interrupted unnecessarily.
    The default values are 25 seconds for `ServerAliveInterval` and
    200 for `ServerAliveCountMax`. If either is set to -1,
    the corresponding SSH option is not set and the value from the SSH
    configuration is used.

--- internal/backend/sftp/sshcmd_test.go
+++ internal/backend/sftp/sshcmd_test.go
@@ -12,54 +12,90 @@ var sshcmdTests = []struct {
 	err  string
 }{
 	{
-		Config{User: "user", Host: "host", Path: "dir/subdir"},
+		Config{User: "user", Host: "host", Path: "dir/subdir", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"ssh",
 		[]string{"host", "-l", "user", "-s", "sftp"},
 		"",
 	},
 	{
-		Config{Host: "host", Path: "dir/subdir"},
+		Config{Host: "host", Path: "dir/subdir", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"ssh",
 		[]string{"host", "-s", "sftp"},
 		"",
 	},
 	{
-		Config{Host: "host", Port: "10022", Path: "/dir/subdir"},
+		Config{Host: "host", Port: "10022", Path: "/dir/subdir", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"ssh",
 		[]string{"host", "-p", "10022", "-s", "sftp"},
 		"",
 	},
 	{
-		Config{User: "user", Host: "host", Port: "10022", Path: "/dir/subdir"},
+		Config{User: "user", Host: "host", Port: "10022", Path: "/dir/subdir", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"ssh",
 		[]string{"host", "-p", "10022", "-l", "user", "-s", "sftp"},
 		"",
 	},
 	{
-		Config{User: "user", Host: "host", Port: "10022", Path: "/dir/subdir", Args: "-i /path/to/id_rsa"},
+		Config{User: "user", Host: "host", Port: "10022", Path: "/dir/subdir", Args: "-i /path/to/id_rsa", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"ssh",
 		[]string{"host", "-p", "10022", "-l", "user", "-i", "/path/to/id_rsa", "-s", "sftp"},
 		"",
 	},
 	{
-		Config{Command: "ssh something", Args: "-i /path/to/id_rsa"},
+		Config{Command: "ssh something", Args: "-i /path/to/id_rsa", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"",
 		nil,
 		"cannot specify both sftp.command and sftp.args options",
 	},
 	{
 		// IPv6 address.
-		Config{User: "user", Host: "::1", Path: "dir"},
+		Config{User: "user", Host: "::1", Path: "dir", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"ssh",
 		[]string{"::1", "-l", "user", "-s", "sftp"},
 		"",
 	},
 	{
 		// IPv6 address with zone and port.
-		Config{User: "user", Host: "::1%lo0", Port: "22", Path: "dir"},
+		Config{User: "user", Host: "::1%lo0", Port: "22", Path: "dir", ServerAliveInterval: -1, ServerAliveCountMax: -1},
 		"ssh",
 		[]string{"::1%lo0", "-p", "22", "-l", "user", "-s", "sftp"},
 		"",
+	},
+	{
+		Config{User: "user", Host: "host", Path: "dir", ServerAliveInterval: 99, ServerAliveCountMax: -1},
+		"ssh",
+		[]string{"host", "-l", "user", "-o", "ServerAliveInterval=99", "-s", "sftp"},
+		"",
+	},
+	{
+		Config{User: "user", Host: "host", Path: "dir", ServerAliveInterval: -1, ServerAliveCountMax: 99},
+		"ssh",
+		[]string{"host", "-l", "user", "-o", "ServerAliveCountMax=99", "-s", "sftp"},
+		"",
+	},
+	{
+		Config{User: "user", Host: "host", Path: "dir", ServerAliveInterval: 99, ServerAliveCountMax: 99},
+		"ssh",
+		[]string{"host", "-l", "user", "-o", "ServerAliveInterval=99", "-o", "ServerAliveCountMax=99", "-s", "sftp"},
+		"",
+	},
+	{
+		Config{User: "user", Host: "host", Path: "dir", ServerAliveInterval: 99, ServerAliveCountMax: 99, Args: "-i /path/to/id_rsa"},
+		"ssh",
+		[]string{"host", "-l", "user", "-o", "ServerAliveInterval=99", "-o", "ServerAliveCountMax=99", "-i", "/path/to/id_rsa", "-s", "sftp"},
+		"",
+	},
+	{
+		Config{User: "user", Host: "host", Path: "dir", ServerAliveInterval: 0, ServerAliveCountMax: 99},
+		"ssh",
+		[]string{"host", "-l", "user", "-o", "ServerAliveInterval=0", "-o", "ServerAliveCountMax=99", "-s", "sftp"},
+		"",
+	},
+	{
+		Config{User: "user", Host: "host", Path: "dir", ServerAliveInterval: 99, ServerAliveCountMax: 0},
+		"",
+		nil,
+		"sftp.server-alive-count-max cannot be 0",
 	},
 }
 
@@ -68,6 +104,9 @@ func TestBuildSSHCommand(t *testing.T) {
 		t.Run("", func(t *testing.T) {
 			cmd, args, err := buildSSHCommand(test.cfg)
 			if test.err != "" {
+				if err == nil {
+					t.Fatalf("expected error %v got nil", test.err)
+				}
 				if err.Error() != test.err {
 					t.Fatalf("expected error %v got %v", test.err, err.Error())
 				}
