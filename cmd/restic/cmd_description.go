ce08f28ff1a8478c2daf83f889f00770985cb6be
Laborratte5 <laborratte5@gmail.com>
2025-04-26 21:23:10 +0200

    Implement runDescription to change the description for a snapshot with the given ID

--- cmd/restic/cmd_description.go
+++ cmd/restic/cmd_description.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 
+	"github.com/restic/restic/internal/debug"
+	"github.com/restic/restic/internal/repository"
+	"github.com/restic/restic/internal/restic"
 	"github.com/restic/restic/internal/ui/termstatus"
 	"github.com/spf13/cobra"
 )
@@ -11,6 +14,47 @@ func newDescriptionCommand() *cobra.Command {
 	return nil
 }
 
+func changeDescription(ctx context.Context, repo *repository.Repository, sn *restic.Snapshot, newDescription string) error {
+	sn.Description = newDescription
+
+	sn.Original = sn.ID()
+	id, err := restic.SaveSnapshot(ctx, repo, sn)
+	if err != nil {
+		return err
+	}
+
+	debug.Log("old snapshot %v saved as new snapshot %v", sn.ID(), id)
+
+	if err = repo.RemoveUnpacked(ctx, restic.WriteableSnapshotFile, *sn.ID()); err != nil {
+		return err
+	}
+
+	debug.Log("old snapshot %v removed", sn.ID())
+
+	return nil
+}
+
 func runDescription(ctx context.Context, snapshot, description string, gopts GlobalOptions, term *termstatus.Terminal, args []string) error {
+
+	Verbosef("create exclusive lock for repository\n")
+	ctx, repo, unlock, err := openWithExclusiveLock(ctx, gopts, false)
+	if err != nil {
+		return err
+	}
+	defer unlock()
+
+	// TODO output, summary usw.
+	for sn := range FindFilteredSnapshots(ctx, repo, repo, &restic.SnapshotFilter{}, []string{snapshot}) {
+		err := changeDescription(ctx, repo, sn, description)
+		if err != nil {
+			Warnf("unable to modify the description for snapshot ID %q, ignoring: %v'\n", sn.ID(), err)
+			continue
+		}
+	}
+
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
 	return nil
 }
