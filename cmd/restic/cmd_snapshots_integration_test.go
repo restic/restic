3bdd1e162ca7fcad63d6848440d62e200e99cdfd
Winfried Plappert <18740761+wplapper@users.noreply.github.com>
2025-07-03 07:11:55 +0100

    SnapshotFilter - time based filtering - more tests
    Added internal/restic/snapshot_find_test.go:
    Duration based testing
    
    cmd/restic/cmd_snapshots_integration_test.go:
    cleaned up logic, added comments consolidated tests

--- cmd/restic/cmd_snapshots_integration_test.go
+++ cmd/restic/cmd_snapshots_integration_test.go
@@ -36,6 +36,7 @@ func testRunSnapshotsOpts(t testing.TB, opts SnapshotOptions, gopts GlobalOption
 	buf, err := withCaptureStdout(func() error {
 		gopts.JSON = true
 
+		// list all snapshots in JSON mode
 		return runSnapshots(context.TODO(), opts, gopts, []string{})
 	})
 	rtest.OK(t, err)
@@ -47,7 +48,7 @@ func testRunSnapshotsOpts(t testing.TB, opts SnapshotOptions, gopts GlobalOption
 	for _, sn := range snapshots {
 		snapmap[*sn.ID] = sn
 	}
-	return
+	return snapmap
 }
 
 func testSetDuration(t *testing.T, duration string) (result restic.DurationTime) {
@@ -63,7 +64,7 @@ func TestSnapshotTimeFilter(t *testing.T) {
 	testSetupBackupData(t, env)
 	opts := BackupOptions{}
 
-	// make 5 backups
+	// create 5 backups which will receive changed backup timestamps
 	for i := 0; i < 5; i++ {
 		testRunBackup(t, env.testdata+"/0", []string{"0/9"}, opts, env.gopts)
 	}
@@ -78,12 +79,14 @@ func TestSnapshotTimeFilter(t *testing.T) {
 		testSetDuration(t, "2m"),
 		testSetDuration(t, "6m"),
 	}
+
+	// convert Durations to timestamps
 	newTimes := make([]restic.DurationTime, len(offsets))
 	for i, offset := range offsets {
 		newTimes[i] = timeReference.AddOffset(offset)
 	}
 
-	// rewrite the backup times for all given backups
+	// rewrite the backup timestamps for all given backups
 	for i, snapID := range snapshotIDs {
 		rewriteOpts := RewriteOptions{
 			Metadata: snapshotMetadataArgs{
@@ -95,20 +98,24 @@ func TestSnapshotTimeFilter(t *testing.T) {
 	}
 	testListSnapshots(t, env.gopts, 5)
 
-	// extract all snapshots and their time
+	// extract all snapshots and their backup timestamps into
+	referenceT := testSetDuration(t, "now")
+	latest := testSetDuration(t, "latest")
+
 	optsSnap := SnapshotOptions{
 		SnapshotFilter: restic.SnapshotFilter{
-			OlderThan:  offsets[0],
-			RelativeTo: timeReference,
+			OlderThan:  latest,
+			RelativeTo: referenceT,
 		},
 	}
 	snapmap := testRunSnapshotsOpts(t, optsSnap, env.gopts)
 	rtest.Assert(t, len(snapmap) == 5, "there should be 5 snapshots in all, but there are %d of them", len(snapmap))
 
-	durationMap := map[int]restic.ID{}
+	// build 'durationMap' so we can find the snapID which belongs to a particular timestamp
+	durationMap := make(map[int]restic.ID, len(snapmap))
 	for ID, sn := range snapmap {
-		for i, newTime := range newTimes {
-			if newTime.GetTime().Equal(sn.Time) {
+		for i, backupTime := range newTimes {
+			if backupTime.GetTime().Equal(sn.Time) {
 				durationMap[i] = ID
 				break
 			}
@@ -118,30 +125,18 @@ func TestSnapshotTimeFilter(t *testing.T) {
 
 	olderThan := offsets[2]
 	newerThan := offsets[3]
+
+	shiftOlder := timeReference.AddOffset(olderThan)
+	shiftNewer := timeReference.AddOffset(newerThan)
+
 	var dtOlder restic.DurationTime
 	var dtNewer restic.DurationTime
 	rtest.OK(t, dtOlder.Set(durationMap[2].String()))
 	rtest.OK(t, dtNewer.Set(durationMap[3].String()))
 
-	shiftOlder := timeReference.AddOffset(olderThan)
-	shiftOlder.SetTime(shiftOlder.GetTime().Add(time.Second))
-	shiftNewer := timeReference.AddOffset(newerThan)
-	shiftNewer.SetTime(shiftNewer.GetTime().Add(-time.Second))
-
-	// if 'RelativeTo' is missing from 'SnapshotOptions', its either because
-	// the other options have a fixed restic.DurationTime or are implicitely
-	// activating RelativeTo = latest, the default setting
-
 	// ================== OlderThan ==================
-	optsSnap = SnapshotOptions{
-		SnapshotFilter: restic.SnapshotFilter{
-			OlderThan:  olderThan,
-			RelativeTo: timeReference,
-		},
-	}
-	snapmap = testRunSnapshotsOpts(t, optsSnap, env.gopts)
-	rtest.Assert(t, len(snapmap) == 3, "there should be 3 snapshots older than 1 month, but there are %d of them", len(snapmap))
-
+	// the 3 variables here represent the same timestamp in the possible 3 formats
+	// a Duration, a timestamp (as calculated from an Duration offset), a snapID from a rewritten backup
 	for _, dt := range []restic.DurationTime{olderThan, shiftOlder, dtOlder} {
 		optsSnap = SnapshotOptions{SnapshotFilter: restic.SnapshotFilter{
 			OlderThan: dt,
@@ -151,15 +146,6 @@ func TestSnapshotTimeFilter(t *testing.T) {
 	}
 
 	// ================== NewerThan ==================
-	optsSnap = SnapshotOptions{
-		SnapshotFilter: restic.SnapshotFilter{
-			NewerThan:  newerThan,
-			RelativeTo: timeReference,
-		},
-	}
-	snapmap = testRunSnapshotsOpts(t, optsSnap, env.gopts)
-	rtest.Assert(t, len(snapmap) == 4, "there should be 4 snapshots newer than 2 months, but there are %d of them", len(snapmap))
-
 	for _, dt := range []restic.DurationTime{newerThan, shiftNewer, dtNewer} {
 		optsSnap = SnapshotOptions{SnapshotFilter: restic.SnapshotFilter{
 			NewerThan: dt,
@@ -173,13 +159,6 @@ func TestSnapshotTimeFilter(t *testing.T) {
 		NewerThan restic.DurationTime
 		OlderThan restic.DurationTime
 	}
-	optsSnap = SnapshotOptions{SnapshotFilter: restic.SnapshotFilter{
-		NewerThan:  newerThan,
-		OlderThan:  olderThan,
-		RelativeTo: timeReference,
-	}}
-	snapmap = testRunSnapshotsOpts(t, optsSnap, env.gopts)
-	rtest.Assert(t, len(snapmap) == 2, "there should be 2 snapshots between 1 and 2 months, but there are %d of them", len(snapmap))
 
 	for _, elem := range []both{{newerThan, olderThan}, {shiftNewer, shiftOlder}, {dtNewer, dtOlder}} {
 		optsSnap = SnapshotOptions{SnapshotFilter: restic.SnapshotFilter{
