aa1ebe91bcbcd65478f7389d048f35f5f94f5364
Laborratte5 <laborratte5@gmail.com>
2025-12-30 17:04:20 +0100

    Allow description command with multiple snapshot ids
    Allow changing the description of multiple snapshots at once.

--- cmd/restic/cmd_description_integration_test.go
+++ cmd/restic/cmd_description_integration_test.go
@@ -11,9 +11,14 @@ import (
 // TODO add tests
 
 func testRunDescription(t testing.TB, description string, gopts global.Options) {
-	withTermStatus(t, gopts, func(ctx context.Context, gopts global.Options) error {
-		return runDescription(context.TODO(), changeDescriptionOptions{descriptionOptions: descriptionOptions{Description: description}}, gopts, []string{"latest"})
+	testRunDescriptionWithArgs(t, description, gopts, []string{"latest"})
+}
+
+func testRunDescriptionWithArgs(t testing.TB, description string, gopts global.Options, args []string) {
+	err := withTermStatus(t, gopts, func(ctx context.Context, gopts global.Options) error {
+		return runDescription(context.TODO(), changeDescriptionOptions{descriptionOptions: descriptionOptions{Description: description}}, gopts, args)
 	})
+	rtest.OK(t, err)
 }
 
 func testRunDescriptionRemove(t testing.TB, gopts global.Options) {
@@ -94,4 +99,46 @@ func TestDescription(t *testing.T) {
 	rtest.Assert(t, *newest.Original == previousId,
 		"expected original ID to be set to the previous snapshot id")
 
+	// Test editing multiple descriptions at once
+	// Create second snapshot
+	first := newest
+	testRunBackup(t, "", []string{env.testdata}, BackupOptions{}, env.gopts)
+	second, _ := testRunSnapshots(t, env.gopts)
+	expectedThirdDescription := "third snapshot description which should not be changed"
+	testRunBackup(t, "", []string{env.testdata}, BackupOptions{DescriptionOptions: descriptionOptions{Description: expectedThirdDescription}}, env.gopts)
+	third, _ := testRunSnapshots(t, env.gopts)
+	rtest.Assert(t, *second.Parent == *first.ID, "expected second snapshot to have third snapshot as parent")
+	rtest.Assert(t, *third.Parent == *second.ID, "expected third snapshot to have second snapshot as parent")
+	// Assert unchanged descriptions
+	rtest.Assert(t, first.Description == "", "expected empty description in first snapshot, got %s", first.Description)
+	rtest.Assert(t, second.Description == "", "expected empty description in second snapshot, got %s", second.Description)
+	rtest.Assert(t, third.Description == expectedThirdDescription,
+		"expected description %s in third snapshot, got %s", expectedThirdDescription, third.Description)
+	// Change description of first and second snapshot at once
+	expectedChangedDescription := "This is a new description for the first and second snapshot."
+	testRunDescriptionWithArgs(t, expectedChangedDescription, env.gopts, []string{first.ShortID, second.ShortID})
+	// Assert changed description in first and second snapshot, unchanged description in third snapshot
+	_, snapmap := testRunSnapshots(t, env.gopts)
+	var changedFirst, changedSecond Snapshot
+	for id, snapshot := range snapmap {
+		if id == *third.ID {
+			// Third snapshot should not change an therefor has no original ID
+			continue
+		}
+		switch *snapshot.Original {
+		case *first.ID:
+			changedFirst = snapshot
+		case *second.ID:
+			changedSecond = snapshot
+		default:
+			t.Fatalf("Unexpected snapshot %v with original %v", snapshot, snapshot.Original)
+		}
+	}
+
+	rtest.Assert(t, changedFirst.Description == expectedChangedDescription,
+		"Expected description '%s' in first snapshot, got '%s'", expectedChangedDescription, changedFirst.Description)
+	rtest.Assert(t, changedSecond.Description == expectedChangedDescription,
+		"Expected description '%s' in second snapshot, got '%s'", expectedChangedDescription, changedSecond.Description)
+	rtest.Assert(t, third.Description == expectedThirdDescription,
+		"Expected description '%s' in third snapshot, got '%s'", expectedChangedDescription, third.Description)
 }
