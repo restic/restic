ca24aae27263be297807902b164be6cf98529c31
Winfried Plappert <18740761+wplapper@users.noreply.github.com>
2026-01-10 16:09:33 +0000

    restic find --pack <tree-pack> did not output
    cmd/restic/cmd_find.go: now produces output for both data-blob and
    tree-blob packfiles.
    In func packsToBlobs() the Type of the blob is now checked and inserts
    the blob either into f.blobIDs or into f.treeIDs.

--- cmd/restic/cmd_find.go
+++ cmd/restic/cmd_find.go
@@ -450,6 +450,9 @@ func (f *Finder) packsToBlobs(ctx context.Context, packs []string) error {
 	if f.blobIDs == nil {
 		f.blobIDs = make(map[string]struct{})
 	}
+	if f.treeIDs == nil {
+		f.treeIDs = make(map[string]struct{})
+	}
 
 	debug.Log("Looking for packs...")
 	err := f.repo.List(ctx, restic.PackFile, func(id restic.ID, size int64) error {
@@ -470,7 +473,11 @@ func (f *Finder) packsToBlobs(ctx context.Context, packs []string) error {
 			return err
 		}
 		for _, b := range blobs {
-			f.blobIDs[b.ID.String()] = struct{}{}
+			if b.Type == restic.DataBlob {
+				f.blobIDs[b.ID.String()] = struct{}{}
+			} else if b.Type == restic.TreeBlob {
+				f.treeIDs[b.ID.String()] = struct{}{}
+			}
 		}
 		// Stop searching when all packs have been found
 		if len(packIDs) == 0 {
@@ -557,7 +564,7 @@ func (f *Finder) findObjectPack(id string, t restic.BlobType) {
 
 	blobs := f.repo.LookupBlob(t, rid)
 	if len(blobs) == 0 {
-		f.printer.S("Object %s not found in the index", rid.Str())
+		f.printer.S("Object %s with type %s not found in the index", t.String(), rid.Str())
 		return
 	}
 
