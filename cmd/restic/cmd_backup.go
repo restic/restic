cbc061a66ec1e45d7f0126787b574549c25f9cd7
astro-stan <36302090+astro-stan@users.noreply.github.com>
2026-02-26 23:17:48 +0000

    Add support for setting `--ignore-ctime` and `--ignore-inode` via environment variables

--- cmd/restic/cmd_backup.go
+++ cmd/restic/cmd_backup.go
@@ -133,8 +133,23 @@ func (opts *BackupOptions) AddFlags(f *pflag.FlagSet) {
 	f.StringArrayVar(&opts.FilesFromRaw, "files-from-raw", nil, "read the files to backup from `file` (can be combined with file args; can be specified multiple times)")
 	f.StringVar(&opts.TimeStamp, "time", "", "`time` of the backup (ex. '2012-11-01 22:08:41') (default: now)")
 	f.BoolVar(&opts.WithAtime, "with-atime", false, "store the atime for all files and directories")
-	f.BoolVar(&opts.IgnoreInode, "ignore-inode", false, "ignore inode number and ctime changes when checking for modified files")
-	f.BoolVar(&opts.IgnoreCtime, "ignore-ctime", false, "ignore ctime changes when checking for modified files")
+
+	// Use environment to set the default value if available
+	ignoreInodeDefault := false
+	if v := os.Getenv("RESTIC_IGNORE_INODE"); v != "" {
+		if b, err := strconv.ParseBool(v); err == nil {
+			ignoreInodeDefault = b
+		}
+	}
+	ignoreCtimeDefault := false
+	if v := os.Getenv("RESTIC_IGNORE_CTIME"); v != "" {
+		if b, err := strconv.ParseBool(v); err == nil {
+			ignoreCtimeDefault = b
+		}
+	}
+
+	f.BoolVar(&opts.IgnoreInode, "ignore-inode", ignoreInodeDefault, "ignore inode number and ctime changes when checking for modified files")
+	f.BoolVar(&opts.IgnoreCtime, "ignore-ctime", ignoreCtimeDefault, "ignore ctime changes when checking for modified files")
 	f.BoolVarP(&opts.DryRun, "dry-run", "n", false, "do not upload or write any data, just show what would be done")
 	f.BoolVar(&opts.NoScan, "no-scan", false, "do not run scanner to estimate size of backup")
 	if runtime.GOOS == "windows" {
