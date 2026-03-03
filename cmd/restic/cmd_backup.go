8b85047b6b7dfd71589ef17f1600a00f02e5ef3d
astro-stan <36302090+astro-stan@users.noreply.github.com>
2026-03-03 20:35:15 +0000

    fix compilation error

--- cmd/restic/cmd_backup.go
+++ cmd/restic/cmd_backup.go
@@ -150,8 +150,8 @@ func (opts *BackupOptions) AddFlags(f *pflag.FlagSet) {
 	opts.ReadConcurrency = uint(readConcurrency)
 
 	// parse read inode and ctime from env, on error the default value will be used
-	opts.IgnoreInode, _ := strconv.ParseBool(os.Getenv("RESTIC_IGNORE_INODE"))
-	opts.IgnoreCtime, _ := strconv.ParseBool(os.Getenv("RESTIC_IGNORE_CTIME"))
+	opts.IgnoreInode, _ = strconv.ParseBool(os.Getenv("RESTIC_IGNORE_INODE"))
+	opts.IgnoreCtime, _ = strconv.ParseBool(os.Getenv("RESTIC_IGNORE_CTIME"))
 
 	// parse host from env, if not exists or empty the default value will be used
 	if host := os.Getenv("RESTIC_HOST"); host != "" {
