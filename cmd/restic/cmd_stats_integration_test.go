4aa228cba35261c3b8461f0a142ccd88d8269e42
Winfried Plappert <18740761+wplapper@users.noreply.github.com>
2026-05-05 06:22:44 +0100

    stats --mode debug - integration test
    add one test to read text from os.stderr
    
    stats --mode debug - logic simplification
    
    the unique files and extension stats profile is now generated for
    all used blobs instead of all blobs known to the index.
    
    stats mode --debug - linter error
    
    forgot to check the error check for calling data.StreamTrees()

---
+++ cmd/restic/cmd_stats_integration_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/restic/restic/internal/global"
+	rtest "github.com/restic/restic/internal/test"
+)
+
+// withCaptureStdoutStderr captures stdout and stderr in a buffer for analysis
+func withCaptureStdoutStderr(t testing.TB, gopts global.Options,
+	callback func(ctx context.Context, gopts global.Options) error,
+) (*bytes.Buffer, *bytes.Buffer, error) {
+
+	bufStdout := bytes.NewBuffer(nil)
+	bufStderr := bytes.NewBuffer(nil)
+	err := withTermStatusRaw(os.Stdin, bufStdout, bufStderr, gopts, callback)
+
+	return bufStdout, bufStderr, err
+}
+
+// testRunStats runs `restic stats` with capturing std and stderr
+func testRunStats(t testing.TB, wantJSON bool, opts StatsOptions, gopts global.Options, args []string,
+) ([]byte, []byte) {
+
+	bufStdout, bufStderr, err := withCaptureStdoutStderr(t, gopts, func(ctx context.Context, gopts global.Options) error {
+		gopts.JSON = wantJSON
+
+		return runStats(ctx, opts, gopts, args, gopts.Term)
+	})
+	rtest.OK(t, err)
+
+	return bufStdout.Bytes(), bufStderr.Bytes()
+}
+
+func TestStatsDebug(t *testing.T) {
+	env, cleanup := withTestEnvironment(t)
+	defer cleanup()
+
+	testSetupBackupData(t, env)
+	// backup of subtree 0/0/9
+	testRunBackup(t, env.testdata, []string{filepath.Join(env.testdata, "0", "0", "9")}, BackupOptions{}, env.gopts)
+
+	optsStats := StatsOptions{countMode: "debug"}
+	// allow multiple reads of different restic file types
+	env.gopts.BackendTestHook = nil
+	stdout, stderr := testRunStats(t, false, optsStats, env.gopts, nil)
+
+	rtest.Equals(t, "", string(stdout), "stdout should be empty")
+	offset := bytes.Index(stderr, []byte("Distinct"))
+	rtest.Assert(t, offset > 0, "did not find the word 'Distinct'")
+	lines := strings.Split(string(stderr[offset:]), "\n")
+	rtest.Assert(t, len(lines) > 11, "expected at least 11 lines of output starting here")
+	rtest.Equals(t, "Count: 69", lines[1])
+	rtest.Equals(t, "10000 - 99999 Byte  69", lines[5])
+	rtest.Equals(t, "file            69", lines[11])
+}
