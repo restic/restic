496ea804a6c9735ee99118da1025e3d0ca082ca6
Michael Eischer <michael.eischer@fau.de>
2026-05-16 11:39:14 +0200

    check/dump/mount/repair/rewrite: update command description

--- cmd/restic/cmd_repair.go
+++ cmd/restic/cmd_repair.go
@@ -7,8 +7,12 @@ import (
 
 func newRepairCommand(globalOptions *global.Options) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:               "repair",
-		Short:             "Repair the repository",
+		Use:   "repair",
+		Short: "Repair the repository",
+		Long: `
+The "repair" command repairs damaged repositories. It provides subcommands to
+rebuild the index, salvage damaged pack files, and repair broken snapshots.
+`,
 		GroupID:           cmdGroupDefault,
 		DisableAutoGenTag: true,
 	}
