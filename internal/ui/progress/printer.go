47328afb2e59e5efb67441bf5e9809ee6af9c81e
Michael Eischer <michael.eischer@fau.de>
2026-06-05 14:21:54 +0200

    ui/progress: remove unused test helper

--- internal/ui/progress/printer.go
+++ internal/ui/progress/printer.go
@@ -1,7 +1,5 @@
 package progress
 
-import "testing"
-
 // A Printer can can return a new counter or print messages
 // at different log levels.
 // It must be safe to call its methods from concurrent goroutines.
@@ -58,48 +56,3 @@ func (*NoopPrinter) P(_ string, _ ...interface{}) {}
 func (*NoopPrinter) V(_ string, _ ...interface{}) {}
 
 func (*NoopPrinter) VV(_ string, _ ...interface{}) {}
-
-// TestPrinter prints messages during testing
-type TestPrinter struct {
-	t testing.TB
-}
-
-func NewTestPrinter(t testing.TB) *TestPrinter {
-	return &TestPrinter{
-		t: t,
-	}
-}
-
-var _ Printer = (*TestPrinter)(nil)
-
-func (p *TestPrinter) NewCounter(_ string) *Counter {
-	return nil
-}
-
-func (p *TestPrinter) NewCounterTerminalOnly(_ string) *Counter {
-	return nil
-}
-
-func (p *TestPrinter) E(msg string, args ...interface{}) {
-	p.t.Logf("error: "+msg, args...)
-}
-
-func (p *TestPrinter) S(msg string, args ...interface{}) {
-	p.t.Logf("stdout: "+msg, args...)
-}
-
-func (p *TestPrinter) PT(msg string, args ...interface{}) {
-	p.t.Logf("stdout(terminal): "+msg, args...)
-}
-
-func (p *TestPrinter) P(msg string, args ...interface{}) {
-	p.t.Logf("print: "+msg, args...)
-}
-
-func (p *TestPrinter) V(msg string, args ...interface{}) {
-	p.t.Logf("verbose: "+msg, args...)
-}
-
-func (p *TestPrinter) VV(msg string, args ...interface{}) {
-	p.t.Logf("verbose2: "+msg, args...)
-}
