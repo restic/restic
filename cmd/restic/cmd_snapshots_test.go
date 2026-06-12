dca7a243693a2fe0c405328c735d926a63e356df
Michael Eischer <michael.eischer@fau.de>
2026-06-12 21:47:38 +0200

    snapshots: add test for group defaulting of --latest

--- cmd/restic/cmd_snapshots_test.go
+++ cmd/restic/cmd_snapshots_test.go
@@ -4,8 +4,38 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/restic/restic/internal/data"
 	rtest "github.com/restic/restic/internal/test"
+	"github.com/spf13/pflag"
 )
+
+func TestSnapshotOptionsFinalize(t *testing.T) {
+	for _, test := range []struct {
+		name string
+		args []string
+		want data.SnapshotGroupByOptions
+	}{
+		{
+			name: "default group by when latest set",
+			args: []string{"--latest", "1"},
+			want: data.SnapshotGroupByOptions{Host: true, Path: true},
+		},
+		{
+			name: "preserve explicit group by",
+			args: []string{"--latest", "1", "--group-by", "host"},
+			want: data.SnapshotGroupByOptions{Host: true},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			set := pflag.NewFlagSet("test", pflag.PanicOnError)
+			opts := &SnapshotOptions{}
+			opts.AddFlags(set)
+			rtest.OK(t, set.Parse(test.args))
+			rtest.OK(t, opts.Finalize())
+			rtest.Equals(t, test.want, opts.GroupBy)
+		})
+	}
+}
 
 // Regression test for #2979: no snapshots should print as [], not null.
 func TestEmptySnapshotGroupJSON(t *testing.T) {
