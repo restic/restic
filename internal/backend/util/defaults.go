024ac27edb25c67d2964b554125fad4f70049bfc
Michael Eischer <michael.eischer@fau.de>
2026-06-20 19:03:34 +0200

    backend/rest: fix flaky test cleanup

--- internal/backend/util/defaults.go
+++ internal/backend/util/defaults.go
@@ -35,7 +35,12 @@ func DefaultDelete(ctx context.Context, be backend.Backend) error {
 
 	for _, t := range alltypes {
 		err := be.List(ctx, t, func(fi backend.FileInfo) error {
-			return be.Remove(ctx, backend.Handle{Type: t, Name: fi.Name})
+			err := be.Remove(ctx, backend.Handle{Type: t, Name: fi.Name})
+			if err != nil && be.IsNotExist(err) {
+				// deletion of files created by TestSaveError may happen with a delay for the REST server, so we ignore the error
+				err = nil
+			}
+			return err
 		})
 		if err != nil {
 			return err
