8b377890c0d1a0b8c9ac494318b88001281ad2ef
Michael Eischer <michael.eischer@fau.de>
2026-07-10 18:54:42 +0200

    archiver: port fileSaver to object.Writer

--- internal/archiver/file_saver.go
+++ internal/archiver/file_saver.go
@@ -4,17 +4,15 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"sync"
 
 	"github.com/restic/restic/internal/data"
 	"github.com/restic/restic/internal/debug"
 	"github.com/restic/restic/internal/errors"
 	"github.com/restic/restic/internal/fs"
+	"github.com/restic/restic/internal/object"
 	"github.com/restic/restic/internal/restic"
 	"golang.org/x/sync/errgroup"
 )
-
-const chunkReadBufSize = 512 * 1024 // matches chunker internal read buffer size
 
 // fileSaver concurrently saves incoming files to the repo.
 type fileSaver struct {
@@ -99,105 +97,20 @@ type saveFileJob struct {
 	complete        fileCompleteFunc
 }
 
-type fileChunkState struct {
-	readBuf []byte
-	bpos    uint
-	bmax    uint
-	closed  bool
-}
-
-func (s *fileChunkState) reset() {
-	s.bpos = 0
-	s.bmax = 0
-	s.closed = false
-}
-
-// readNextChunk reads from rd and returns the next chunk of data. io.EOF is
-// returned when all chunks have been read.
-func (s *fileChunkState) readNextChunk(rd io.Reader, chnker restic.Chunker, data []byte) ([]byte, error) {
-	data = data[:0]
-	for {
-		if s.bpos >= s.bmax {
-			n, err := io.ReadFull(rd, s.readBuf)
-
-			if err == io.ErrUnexpectedEOF {
-				err = nil
-			}
-
-			// io.EOF only happens when the end of the file has been reached.
-			// If this is the case, we need to return the data we have read so far.
-			if err == io.EOF && !s.closed {
-				s.closed = true
-
-				if len(data) > 0 {
-					return data, nil
-				}
-			}
-
-			if err != nil {
-				return nil, err
-			}
-
-			s.bpos = 0
-			s.bmax = uint(n)
-		}
-
-		split := chnker.NextSplitPoint(s.readBuf[s.bpos:s.bmax])
-		if split == -1 {
-			data = append(data, s.readBuf[s.bpos:s.bmax]...)
-			s.bpos = s.bmax
-		} else {
-			data = append(data, s.readBuf[s.bpos:s.bpos+uint(split)]...)
-			s.bpos += uint(split)
-			return data, nil
-		}
-	}
-}
-
 // saveFile stores the file f in the repo, then closes it.
-func (s *fileSaver) saveFile(ctx context.Context, chnker restic.Chunker, chunkState *fileChunkState, snPath string, target string, f fs.File, start func(), finishReading func(), finish func(res futureNodeResult)) {
+func (s *fileSaver) saveFile(ctx context.Context, chnker restic.Chunker, snPath string, target string, f fs.File, start func(), finishReading func(), finish func(res futureNodeResult)) {
 	start()
 
 	fnr := futureNodeResult{
 		snPath: snPath,
 		target: target,
 	}
-	var lock sync.Mutex
-	remaining := 0
-	isCompleted := false
 
-	completeBlob := func() {
-		lock.Lock()
-		defer lock.Unlock()
-
-		remaining--
-		if remaining == 0 && fnr.err == nil {
-			if isCompleted {
-				panic("completed twice")
-			}
-			for _, id := range fnr.node.Content {
-				if id.IsNull() {
-					panic("completed file with null ID")
-				}
-			}
-			isCompleted = true
-			finish(fnr)
-		}
-	}
 	completeError := func(err error) {
-		lock.Lock()
-		defer lock.Unlock()
-
-		if fnr.err == nil {
-			if isCompleted {
-				panic("completed twice")
-			}
-			isCompleted = true
-			fnr.err = fmt.Errorf("failed to save %v: %w", target, err)
-			fnr.node = nil
-			fnr.stats = ItemStats{}
-			finish(fnr)
-		}
+		fnr.err = fmt.Errorf("failed to save %v: %w", target, err)
+		fnr.node = nil
+		fnr.stats = ItemStats{}
+		finish(fnr)
 	}
 
 	debug.Log("%v", snPath)
@@ -216,93 +129,35 @@ func (s *fileSaver) saveFile(ctx context.Context, chnker restic.Chunker, chunkSt
 	}
 
 	chnker.Reset()
-	chunkState.reset()
-
-	node.Content = []restic.ID{}
-	node.Size = 0
-	var idx int
-	for {
-		buf := s.uploader.BlobBufferPool().Get()
-		chunkData, err := chunkState.readNextChunk(f, chnker, buf.Data)
-		if err == io.EOF {
-			buf.Release()
-			break
-		}
-		if err != nil {
-			buf.Release()
-			_ = f.Close()
-			completeError(err)
-			return
-		}
-
-		// put result buffer back for later reuse
-		buf.Data = chunkData
-		chunkSize := len(buf.Data)
-		node.Size += uint64(chunkSize)
-
-		// test if the context has been cancelled, return the error
-		if ctx.Err() != nil {
-			buf.Release()
-			_ = f.Close()
-			completeError(ctx.Err())
-			return
-		}
-
-		// add a place to store the saveBlob result
-		pos := idx
-
-		lock.Lock()
-		node.Content = append(node.Content, restic.ID{})
-		lock.Unlock()
-
-		s.uploader.SaveBlobAsync(ctx, restic.DataBlob, buf, restic.ID{}, false, func(newID restic.ID, known bool, sizeInRepo int, err error) {
-			if err != nil {
-				completeError(err)
-				return
-			}
-
-			lock.Lock()
-			if !known {
-				fnr.stats.DataBlobs++
-				fnr.stats.DataSize += uint64(chunkSize)
-				fnr.stats.DataSizeInRepo += uint64(sizeInRepo)
-			}
-			node.Content[pos] = newID
-			lock.Unlock()
-
-			completeBlob()
-		})
-		idx++
-
-		// test if the context has been cancelled, return the error
-		if ctx.Err() != nil {
-			_ = f.Close()
-			completeError(ctx.Err())
-			return
-		}
-
-		s.CompleteBlob(uint64(chunkSize))
+	writer := object.NewWriter(ctx, restic.DataBlob, chnker, s.uploader)
+	writer.CompleteBlob = s.CompleteBlob
+	size, err := io.Copy(writer, f)
+	if err != nil {
+		_ = f.Close()
+		completeError(err)
+		return
 	}
-
+	finishReading()
 	err = f.Close()
 	if err != nil {
 		completeError(err)
 		return
 	}
-
-	fnr.node = node
-	lock.Lock()
-	// require one additional completeFuture() call to ensure that the future only completes
-	// after reaching the end of this method
-	remaining += idx + 1
-	lock.Unlock()
-	finishReading()
-	completeBlob()
+	writer.FlushAsync(func(ids restic.IDs, err error) {
+		if err != nil {
+			completeError(err)
+			return
+		}
+		fnr.node = node
+		fnr.node.Content = ids
+		fnr.node.Size = uint64(size)
+		fnr.stats.DataBlobs, fnr.stats.DataSize, fnr.stats.DataSizeInRepo = writer.Stats()
+		finish(fnr)
+	})
 }
 
 func (s *fileSaver) worker(ctx context.Context, jobs <-chan saveFileJob) {
 	chnker := s.chunkerFactory.NewChunker()
-	chunkState := &fileChunkState{readBuf: make([]byte, chunkReadBufSize)}
 
 	for {
 		var job saveFileJob
@@ -316,7 +171,7 @@ func (s *fileSaver) worker(ctx context.Context, jobs <-chan saveFileJob) {
 			}
 		}
 
-		s.saveFile(ctx, chnker, chunkState, job.snPath, job.target, job.file, job.start, func() {
+		s.saveFile(ctx, chnker, job.snPath, job.target, job.file, job.start, func() {
 			if job.completeReading != nil {
 				job.completeReading()
 			}
